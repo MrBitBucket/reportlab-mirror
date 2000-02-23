2bab1c5f18c9f747a7d6a971ac0f29e1ec68119f
rgbecker <unknown>
2000-02-23 13:16:56 +0000

    New infrastructure

---
+++ utils/daily.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+###############################################################################
+#
+#	ReportLab Public License Version 1.0
+#
+#   Except for the change of names the spirit and intention of this
+#   license is the same as that of Python
+#
+#	(C) Copyright ReportLab Inc. 1998-2000.
+#
+#
+# All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted, provided
+# that the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation, and that the name of ReportLab not be used
+# in advertising or publicity pertaining to distribution of the software
+# without specific, written prior permission. 
+# 
+#
+# Disclaimer
+#
+# ReportLab Inc. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+# SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS,
+# IN NO EVENT SHALL ReportLab BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE. 
+#
+###############################################################################
+#	$Log: daily.py,v $
+#	Revision 1.1  2000/02/23 13:16:56  rgbecker
+#	New infrastructure
+#
+__version__=''' $Id: daily.py,v 1.1 2000/02/23 13:16:56 rgbecker Exp $ '''
+'''
+script for creating daily cvs archive dump
+'''
+import os, sys, string, traceback, re
+
+#this is where we extract files etc
+groupdir=os.path.normcase(os.path.normpath('/home/groups/ftp/pub/reportlab'))
+projdir = os.path.normcase(os.path.normpath('reportlab'))
+cvsdir = os.path.join(groupdir,projdir)
+release=0		#1 if making a release
+USER='rgbecker'
+
+def get_path():
+	for i in os.environ.keys():
+		if string.upper(i)=='PATH':
+			return string.split(os.environ[i],os.pathsep)
+	return []
+
+def find_exe(exe):
+	if sys.platform=='win32':
+		exe='%s.exe'%exe
+
+	for p in get_path():
+		f = os.path.join(p,exe)
+		if os.path.isfile(f): return f
+
+	return None
+
+def recursive_rmdir(d):
+	'destroy directory d'
+	if os.path.isdir(d):
+		for p in os.listdir(d):
+			fn = os.path.join(d,p)
+			if os.path.isfile(fn):
+				os.remove(fn)
+			else:
+				recursive_rmdir(fn)
+		os.rmdir(d)
+
+def safe_remove(p):
+	if os.path.isfile(p): os.remove(p)
+
+def do_exec(cmd, cmdname):
+	i=os.popen(cmd,'r')
+	print i.read()
+	i = i.close()
+	if i is not None:
+		print 'there was an error executing '+cmdname
+		sys.exit(1)
+
+def cvs_checkout(d):
+	os.chdir(d)
+	recursive_rmdir(cvsdir)
+
+	cvs = find_exe('cvs')
+	if cvs is None:
+		print "Can't find cvs anywhere on the path"
+		os.exit(1)
+
+	os.environ['CVSROOT']=':pserver:%s@cvs1:/cvsroot/reportlab' % USER
+	if release:
+		do_exec(cvs+' tag %s' % release, 'the tag phase')
+
+	do_exec(cvs+' co reportlab', 'the download phase')
+
+def do_zip(d):
+	'create .tgz and .zip file archives of d/reportlab'
+	def find_src_files(L,d,N):
+		if string.upper(os.path.basename(d))=='CVS': return	#ignore all CVS
+		for n in N:
+			fn = os.path.normcase(os.path.normpath(os.path.join(d,n)))
+			if os.path.isfile(fn): L.append(fn)
+
+	os.chdir(d)
+	src_files = []
+	os.path.walk(projdir,find_src_files,src_files)
+	if release:
+		b = "reportlab_%4d%02d%02d" %time.gmtime(time.time())[:3]
+	else:
+		b = "current"
+
+	tarfile = '%s/%s.tgz' % (groupdir,b)
+	zipfile = '%s/%s.zip' % (groupdir,b)
+	safe_remove(zipfile)
+	safe_remove(tarfile)
+	if src_files==[]: return
+	src_files = string.join(src_files)
+
+	tar = find_exe('tar')
+	do_exec('%s czvf %s %s' % (tar, tarfile, src_files), 'tar creation')
+
+	zip = find_exe('zip')
+	do_exec('%s -u %s %s' % (zip, zipfile, src_files), 'zip creation')
+	recursive_rmdir(cvsdir)
+
+	if release:
+		# make links to the latest outcome
+		for b in ['reportlab','current']:
+			ltarfile = '%s/%s.tgz' % (groupdir,b)
+			lzipfile = '%s/%s.zip' % (groupdir,b)
+			safe_remove(lzipfile)
+			safe_remove(ltarfile)
+			os.symlink(zipfile,lzipfile)
+			os.symlink(tarfile,ltarfile)
+
+if __name__=='__main__':
+	release = '-release' in sys.argv[1:]
+	if release:
+		if sys.argv.len()!=3 or sys.argv[1]!='-release':
+			print 'Usage:\n    python daily.py [-release tag]'
+			sys.exit(1)
+		release=sys.argv[2]
+	cvs_checkout(groupdir)
+	do_zip(groupdir)
---
+++ utils/runtests.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+###############################################################################
+#
+#	ReportLab Public License Version 1.0
+#
+#   Except for the change of names the spirit and intention of this
+#   license is the same as that of Python
+#
+#	(C) Copyright ReportLab Inc. 1998-2000.
+#
+#
+# All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted, provided
+# that the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation, and that the name of ReportLab not be used
+# in advertising or publicity pertaining to distribution of the software
+# without specific, written prior permission. 
+# 
+#
+# Disclaimer
+#
+# ReportLab Inc. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+# SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS,
+# IN NO EVENT SHALL ReportLab BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE. 
+#
+###############################################################################
+#	$Log: runtests.py,v $
+#	Revision 1.1  2000/02/23 13:16:56  rgbecker
+#	New infrastructure
+#
+#	
+__version__=''' $Id: runtests.py,v 1.1 2000/02/23 13:16:56 rgbecker Exp $ '''
+'''
+script for testing ReportLab
+'''
+
+_globals=globals().copy()			#make a copy of out globals
+_globals['__name__'] = "__main__"	#for passing to execfile
+
+import os, sys, string, traceback, re
+
+#this is what we need to write to .cvspass for anonymous access
+_testdir=os.path.normcase(os.path.normpath('/tmp/reportlab_test/reportlab'))
+_ecount = 0
+
+def find_py_files(d):
+	def _py_files(L,d,N):
+ 		for n in filter(lambda x: x[-3:]=='.py', N):
+			fn = os.path.normcase(os.path.normpath(os.path.join(d,n)))
+			if os.path.isfile(fn): L.append(fn)
+	L=[]
+	os.path.walk(d,_py_files,L)
+	return L
+
+def find_executable_py_files(d):
+	prog=re.compile('^( |\t)*if( |\t)+__name__( |\t)*==( |\t)*(\'|\")__main__(\'|\")( |\t)*:')
+	L=[]
+	for n in find_py_files(d):
+		for l in open(n,'r').readlines():
+			if prog.match(l) is not None:
+				L.append(n)
+				break
+	return L
+
+def do_tests(d,cyc):
+	global _ecount
+
+	def find_test_files(L,d,N):
+		n = os.path.basename(d)
+		if n!='test' : return
+		for n in filter(lambda n: n[-3:]=='.py',N):
+			fn = os.path.normcase(os.path.normpath(os.path.join(d,n)))
+			if os.path.isfile(fn): L.append(fn)
+
+	if cyc is not None: import Cyclops
+	if d not in sys.path: sys.path.insert(0,d)
+	os.chdir(d)
+	test_files = []
+	os.path.walk('.',find_test_files,test_files)
+	for t in find_executable_py_files('.'):
+		if t not in test_files:
+			test_files.append(t)
+
+	for t in test_files:
+		fn =os.path.normcase(os.path.normpath(os.path.join(d,t)))
+		bn = os.path.basename(fn)
+		print '##### Test %s starting' % bn
+		try:
+			os.chdir(os.path.dirname(fn))
+			if cyc is not None:
+				z = Cyclops.CycleFinder()
+				z.run(execfile,(fn,_globals.copy()))
+				if z.find_cycles():
+					print '!!!!!! CYCLES WERE FOUND !!!!!!'
+					if 's' in cyc: z.show_stats()
+					if 'c' in cyc: z.show_cycles()
+					if 'o' in cyc: z.show_cycleobjs()
+					if 'C' in cyc: z.show_sccs()
+					if 'a' in cyc: z.show_arcs()
+				else:
+					print '!!!!!! NO CYCLES WERE FOUND !!!!!!'
+					if 's' in cyc: z.show_stats()
+			else:
+				execfile(fn,_globals.copy())
+			print '##### Test %s finished ok' % bn
+		except:
+			traceback.print_exc(None,sys.stdout)
+			_ecount = _ecount + 1
+
+def clean_files(d):
+	def find_cleanable_files(L,d,N):
+		n = os.path.basename(d)
+		for n in filter(lambda n: n[-3:]=='.PYC' or n[-4:]=='.PDF' or n[-4:]=='.A85',map(string.upper,N)):
+			fn = os.path.normcase(os.path.normpath(os.path.join(d,n)))
+			if os.path.isfile(fn): os.remove(fn)
+	os.chdir(d)
+	os.path.walk('.',find_cleanable_files,test_files)
+
+if __name__=='__main__':
+	legal_options = ['-cycles', '-dir', '-help','-notest','-clean', '-fclean']
+	def usage(code=0, msg=''):
+		f = code and sys.stderr or sys.stdout
+		if msg is not None: f.write(msg+'\n')
+		f.write(\
+'''
+Usage
+    python runtests.py [options]
+
+    option
+    -help           print this message and exit
+    -dir path       directory to test default /tmp/reportlab_test/reportlab
+    -cycles[=flags] check for cycles using Tim Peter's Cyclops cycle finder
+                    flags=s(tats), a(rcs), C(omponents), c(ycles), o(bjects)
+    -notest         don't carry out tests
+    -clean          cleanup if no errors
+    -fclean         cleanup even if some errors occur
+''')
+		sys.exit(code)
+
+	dir=_testdir
+	dflag = 0
+	cyc = None
+	options = sys.argv[1:]
+	sys.argv[1:]=[]
+	for k in options:
+		if '=' in k:
+			i, cyc = string.split(k,'=')
+			if i=='-cycles': k = i
+			for i in cyc:
+				if i not in 'scCao':
+					usage(code=1,msg="Bad cycles option '%s'"%i)
+		elif k=='-cycles': cyc = 'sc'
+		if k not in legal_options:
+			if dflag:
+				dir = k
+				dflag = 0
+			else:
+				usage(code=1,msg="unknown option '%s'" % k)
+		else:
+			dflag = k=='-dir'
+
+	dir = os.path.normcase(os.path.normpath(dir))
+	if not os.path.isdir(dir):
+		usage(code=1,msg="Invalid directory '%s'"%dir)
+
+	if '-help' in options: usage()
+
+	if '-notest' not in options:
+		do_tests(dir, cyc)
+
+	if ((_ecount==0 and '-clean' in options) or '-fclean' in options):
+		clean_files(dir)
