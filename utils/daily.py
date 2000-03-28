517426713b178d34af493eef092f89adb53ff632
rgbecker <unknown>
2000-03-28 13:40:32 +0000

    Fix server for release

--- utils/daily.py
+++ utils/daily.py
@@ -32,13 +32,16 @@
 #
 ###############################################################################
 #	$Log: daily.py,v $
+#	Revision 1.3  2000/03/28 13:40:32  rgbecker
+#	Fix server for release
+#
 #	Revision 1.2  2000/03/28 13:34:26  rgbecker
 #	Fixed to release mechanism len()?
-#
+#	
 #	Revision 1.1  2000/02/23 13:16:56  rgbecker
 #	New infrastructure
 #	
-__version__=''' $Id: daily.py,v 1.2 2000/03/28 13:34:26 rgbecker Exp $ '''
+__version__=''' $Id: daily.py,v 1.3 2000/03/28 13:40:32 rgbecker Exp $ '''
 '''
 script for creating daily cvs archive dump
 '''
@@ -98,9 +101,11 @@ def cvs_checkout(d):
 		print "Can't find cvs anywhere on the path"
 		os.exit(1)
 
-	os.environ['CVSROOT']=':pserver:%s@cvs1:/cvsroot/reportlab' % USER
 	if release:
+		os.environ['CVSROOT']=':ext:%s@cvs1:/cvsroot/reportlab' % USER
 		do_exec(cvs+' tag %s' % release, 'the tag phase')
+	else:
+		os.environ['CVSROOT']=':pserver:%s@cvs1:/cvsroot/reportlab' % USER
 
 	do_exec(cvs+' co reportlab', 'the download phase')
 
