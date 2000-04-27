a8f1901797460ff3abd95ca9bb7ceef6baedfa2c
rgbecker <unknown>
2000-04-27 10:46:34 +0000

    viewonly acroread options

--- reportlab/demos/py2pdf/idle_print.py
+++ reportlab/demos/py2pdf/idle_print.py
@@ -1,11 +1,12 @@
 # idle_print [py2pdf_options] filename
-__version__=''' $Id: idle_print.py,v 1.4 2000/04/26 13:00:17 rgbecker Exp $ '''
+__version__=''' $Id: idle_print.py,v 1.5 2000/04/27 10:46:34 rgbecker Exp $ '''
 # you should adjust the globals below to configure for your system
 
 import sys, os, py2pdf, string, time
 #whether we remove input/output files; if you get trouble on windows try setting _out to 0
 auto_rm_in	= 1
 auto_rm_out = 1
+viewOnly = 0
 
 #how to call up your acrobat reader
 if sys.platform=='win32':
@@ -14,23 +15,31 @@ if sys.platform=='win32':
 		args = [acrord, pdfname]
 		os.spawnv(os.P_WAIT, args[0], args)
 else:
-	acrord = '/opt/Acrobat4/bin/acroread'
+	acrord = 'acroread'
 	def printpdf(pdfname):
-		cmd = "%s -toPostScript < %s | lpr" % (acrord,pdfname)
+		if viewOnly:
+			cmd = "%s %s" % (acrord,pdfname)
+		else:
+			cmd = "%s -toPostScript < %s | lpr" % (acrord,pdfname)
 		os.system(cmd)
 
 args = ['--format=pdf']
 files = []
 for f in sys.argv[1:]:
 	if f[:2]=='--':
-		if f[2:]=='no_auto_rm_in':
+		opt = f[2:]
+		if opt =='no_auto_rm_in':
 			auto_rm_in = 0
-		elif f[2:]=='auto_rm_in':
+		elif opt =='auto_rm_in':
 			auto_rm_in = 1
-		elif f[2:]=='no_auto_rm_out':
+		elif opt =='no_auto_rm_out':
 			auto_rm_out = 0
-		elif f[2:]=='auto_rm_out':
+		elif opt =='auto_rm_out':
 			auto_rm_out = 1
+		elif opt =='viewonly':
+			viewOnly = 1
+		elif opt[:9] =='acroread=':
+			acrord = opt[9:]
 		else:
 			args.append(f)
 	else: files.append(f)
