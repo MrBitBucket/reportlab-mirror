afc1749eb960401fb9eaa661e47e2bbef06bd63b
andy_robinson <unknown>
2000-06-11 21:34:01 +0000

    Largely complete class for numbering lists, figures and chapters

--- reportlab/lib/sequencer.py
+++ reportlab/lib/sequencer.py
@@ -31,39 +31,46 @@
 #
 ###############################################################################
 #	$Log: sequencer.py,v $
+#	Revision 1.3  2000/06/11 21:34:01  andy_robinson
+#	Largely complete class for numbering lists, figures and chapters
+#
 #	Revision 1.2  2000/06/09 16:18:19  andy_robinson
 #	Doc strings, sequencer
-#
+#	
 #	Revision 1.1  2000/06/01 15:23:06  rgbecker
 #	Platypus re-organisation
 #	
-__version__=''' $Id: sequencer.py,v 1.2 2000/06/09 16:18:19 andy_robinson Exp $ '''
+__version__=''' $Id: sequencer.py,v 1.3 2000/06/11 21:34:01 andy_robinson Exp $ '''
+"""This module defines a single public class, Sequencer, which aids in
+numbering and formatting lists."""
 
-
-def format_123(num):
+def _format_123(num):
 	"""The simplest formatter"""
 	return str(num)
 
-def format_ABC(num):
+def _format_ABC(num):
 	"""Uppercase.  Wraps around at 26."""
 	n = (num -1) % 26
 	return chr(n+65)
 
-def format_abc(num):
+def _format_abc(num):
 	"""Lowercase.  Wraps around at 26."""
 	n = (num -1) % 26
 	return chr(n+97)
 
 
-class Counter:
+class _Counter:
 	"""Private class used by Sequencer.  Each counter
 	knows its format, and the IDs of anything it
 	resets, as well as its value. """
 	def __init__(self):
 		self._base = 1
-		self._value = self.base
+		self._value = self._base
 		self._formatter = format_123
 		self._resets = []
+
+	def setFormatter(self, formatFunc):
+		self._formatter = formatFunc
 
 	def reset(self, value=None):
 		if value:
@@ -74,16 +81,32 @@ class Counter:
 	def next(self):
 		v = self._value
 		self._value = self._value + 1
+		for counter in self._resets:
+			counter.reset()
 		return v
 
 	def this(self):
 		return self._value
 
+	def nextf(self):
+		"""Returns next value formatted"""
+		return self._formatter(self.next())
+
+	def thisf(self):
+		return self._formatter(self.this())
+
+	def chain(self, otherCounter):
+		if not otherCounter in self._resets:
+			self._resets.append(otherCounter)
+
 		
 class Sequencer:
 	"""Something to make it easy to number paragraphs, sections,
-	images and anything else.  It keeps track of a number of
-	'counters'.
+	images and anything else.  The features include registering
+	new string formats for sequences, and 'chains' whereby
+	some counters are reset when their parents.
+	It keeps track of a number of
+	'counters', which are created on request:
 	Usage:
 		>>> seq = layout.Sequencer()
 		>>> seq.next('Bullets')
@@ -104,47 +127,91 @@ class Sequencer:
 		self._counters = {}  #map key to current number
 		self._defaultCounter = None
 
+		self._formatters = {
+			# the formats it knows initially
+			'1':_format_123,
+			'A':_format_ABC,
+			'a':_format_abc
+			}
+
+
 	def _getCounter(self, counter=None):
 		"""Creates one if not present"""
 		try:
-			cnt = self._counters[counter]
+			return self._counters[counter]
 		except KeyError:
 			cnt = _Counter()
 			self._counters[counter] = cnt
-		return cnt
-	
+			return cnt
+
+		
 
 	def this(self, counter=None):
 		"""Retrieves counter value but does not increment. For
 		new counters, sets base value to 1."""
 		if not counter:
 			counter = self._defaultCounter
-		return self._getCounter(counter).value
+		return self._getCounter(counter).this()
 		
 	def next(self, counter=None):
 		"""Retrieves the numeric value for the given counter, then
 		increments it by one.  New counters start at one."""
 		if not counter:
 			counter = self._defaultCounter
-		cnt = self._getCounter(counter)
-		n = cnt.value
-		cnt.value = cnt.value + 1
-		return n
+		return self._getCounter(counter).next()
+
+	def thisf(self, counter=None):
+		if not counter:
+			counter = self._defaultCounter
+		return self._getCounter(counter).thisf()
+	
+	def nextf(self, counter=None):
+		"""Retrieves the numeric value for the given counter, then
+		increments it by one.  New counters start at one."""
+		if not counter:
+			counter = self._defaultCounter
+		return self._getCounter(counter).nextf()
 	
 	def setDefaultCounter(self, default=None):
 		"""Changes the key used for the default"""
 		self._defaultCounter = default
-	
+
+	def registerFormat(self, format, func):
+		"""Registers a new formatting function.  The funtion
+		must take a number as argument and return a string;
+		fmt is a short menmonic string used to access it."""
+		self._formatters[format] = func
+
+	def setFormat(self, counter, format):
+		"""Specifies that the given counter should use
+		the given format henceforth."""
+		func = self._formatters[format]
+		self._getCounter(counter).setFormatter(func)
+		
 	def reset(self, counter=None, base=1):
 		if not counter:
 			counter = self._defaultCounter
 		self._getCounter(counter).value = base
 
-	def _format(self, number, formatCode):
-		pass
-
-	def chain(self, parent, child, base):
+	def chain(self, parent, child):
+		p = self._getCounter(parent)
+		c = self._getCounter(child)
+		p.chain(c)
 		
+
+	def __getitem__(self, key):
+		"""Allows compact notation to support the format function.
+		s['key'] gets current value, s['key+'] increments."""
+		if key[-1:] == '+':
+			counter = key[:-1]
+			return self.nextf(counter)
+		else:
+			return self.thisf(key)
+
+	def format(self, template):
+		"""The crowning jewels - formats multi-level lists."""
+		return template % self
+	
 def test():
 	s = Sequencer()
 	print 'Counting using default sequence: %d %d %d' % (s.next(),s.next(), s.next())
@@ -156,8 +223,46 @@ def test():
 	s.setDefaultCounter()
 	print 'Set default to None again: %d %d %d' % (s.next(),s.next(), s.next())
 	print
+	print 'Creating Appendix counter with format A, B, C...'
+	s.setFormat('Appendix', 'A')
+	print '    Appendix %s, Appendix %s, Appendix %s' % (
+		s.nextf('Appendix'),	s.nextf('Appendix'),s.nextf('Appendix'))
+
+	def format_french(num):
+		return ('un','deux','trois','quatre','cinq')[(num-1)%5]
+	print
+	print 'Defining a custom format with french words:'
+	s.registerFormat('french', format_french)
+	s.setFormat('FrenchList', 'french')
+	print '   ',
+	for i in range(1,6):
+		print s.nextf('FrenchList'),
+	print
+	print 'Chaining H1 and H2 - H2 goes back to one when H1 increases'
+	s.chain('H1','H2')
+	print '    H1 = %d' % s.next('H1')
+	print '      H2 = %d' % s.next('H2')
+	print '      H2 = %d' % s.next('H2')
+	print '      H2 = %d' % s.next('H2')
+	print '    H1 = %d' % s.next('H1')
+	print '      H2 = %d' % s.next('H2')
+	print '      H2 = %d' % s.next('H2')
+	print '      H2 = %d' % s.next('H2')
+	print
+	print 'GetItem notation - append a plus to increment'
+	print '    seq["Appendix"] = %s' % s["Appendix"]
+	print '    seq["Appendix+"] = %s' % s["Appendix+"]
+	print '    seq["Appendix+"] = %s' % s["Appendix+"]
+	print '    seq["Appendix"] = %s' % s["Appendix"]
+	print
+	print 'Finally, string format notation for nested lists.  Cool!'
+	print 'The expression ("Figure %(Chapter)s.%(Figure+)s" % seq) gives:'
+	print '    Figure %(Chapter)s.%(Figure+)s' % s
+	print '    Figure %(Chapter)s.%(Figure+)s' % s 
+	print '    Figure %(Chapter)s.%(Figure+)s' % s 
 	
 	
+		
 if __name__=='__main__':
 	test()
 
