e27bd2cbf83b48f182fb8f9d9201337aef6af961
rgbecker <unknown>
2000-07-13 13:23:01 +0000

    More demos in appendix

--- docs/userguide/app_demos.py
+++ docs/userguide/app_demos.py
@@ -22,17 +22,17 @@ cd reportlab/demos/odyssey
 python odyssey.py
 acrord odyssey.pdf
 """)
-disc("""Simple formatting is illustrated by the odyssey.py script. It runs quite fast,
+disc("""Simple formatting is shown by the odyssey.py script. It runs quite fast,
 but all it does is gather the text and force it onto the canvas pages. It does no paragraph
 manipulation at all so you get to see the XML &lt;  &amp; &gt; tags.
 """)
 disc("""The scripts fodyssey.py and dodussey.py handle paragraph formatting so you get
 to see colour changes etc. Both scripts
 use the document template class and the dodyssey.py script shows the ability to do dual column
-layout and illustrates multiple page templates.
+layout and uses multiple page templates.
 """)
 
-heading2("""Stdfonts""")
+heading2("""Standard Fonts and Colors""")
 disc("""In reportlab/demos/stdfonts the script stdfonts.py can be used to illustrate
 ReportLab's standard fonts. Run the script using""")
 eg("""
@@ -41,11 +41,18 @@ python stdfonts.py
 """)
 disc("""
 to produce two <b>PDF</b> documents, StandardFonts_MacRoman.pdf &amp;
-StandardFonts_WinAnsi.pdf which illustrate the two most common built in
+StandardFonts_WinAnsi.pdf which show the two most common built in
 font encodings.
 """)
+heading3("""Colors""")
+disc("""The colortest.py script in reportlab/demos/colors demonstrates the different ways in which
+reportlab can set up and use colors.""")
+disc("""Try running the script and viewing the output document, colortest.pdf. This shows
+different color spaces and a large selection of the colors which are named
+in the reportlab.lib.colors module.
+""")
 heading2("""Py2pdf""")
-disc("""Dinu Gherman (<gherman@darwin.in-berlin.de>) contributed this useful script
+disc("""Dinu Gherman (&lt;gherman@darwin.in-berlin.de&gt;) contributed this useful script
 which uses reportlab to produces nicely colorized pdf documents from python
 scripts. To get a nice version of the main script try""")
 eg("""
@@ -55,4 +62,50 @@ acrord py2pdf.pdf
 """)
 disc("""ie we used py2pdf to produce a nice version of py2pdf.py in
 the document with the same rootname and a .pdf extension.
+""")
+disc("""
+The py2pdf.py script has many options which are beyond the scope of this simple introduction
+consult the comments at the start of the script.
+""")
+heading2("Gadflypaper")
+disc("""
+The Python script, gfe.py, in reportlab/demos/gadflypaper uses an inline style of
+document preparation. The script almost entirely produced by Aaron Watters produces a document
+describing Aaron's $gadfly$ in memory database for Python. To generate the document use
+""")
+eg("""
+cd reportlab\\gadflypaper
+python gfe.py
+start gfe.pdf
+""")
+disc("""
+everything in the PDF document was produced by the script which is why this is an inline style
+of document production. So to produce a header followed by some text the script uses functions
+$header$ and $p$ which take some text and append to a global story list.
+""")
+eg('''
+header("Conclusion")
+
+p("""The revamped query engine design in Gadfly 2 supports
+..........
+and integration.""")
+''')
+heading2("""Pythonpoint""")
+disc("""Andy Robinson has refined the pythonpoint.py script (in reportlab\\demos\\pythonpoint)
+until it is a really useful script. It takes an input file containing an XML markup
+and uses an xmllib style parser to map the tags into PDF slides. When run in its own directory
+pythonpoint.py takes as a default input the file pythonpoint.xml and produces pythonpoint.pdf
+which is documentation for Pythonpoint! You can also see it in action with an older paper
+""")
+eg("""
+cd reportlab\\demos\\pythonpoint
+python pythonpoint.py monterey.xml
+start monterey.pdf
+""")
+disc("""
+Not only is pythonpoint self documenting, but it also demonstrates reportlab and PDF. It uses
+many features of reportlab (document templates, tables etc).
+Exotic features of PDF such as fadeins and bookmarks are also shown to good effect. The use of
+an XML document can be contrasted with the <i>inline</i> style of the gadflypaper demo; the
+content is completely separate from the formatting
 """)
