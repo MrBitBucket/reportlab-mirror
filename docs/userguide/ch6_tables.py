94a540de0709b68a76a468818f63020ecbc5a443
rgbecker <unknown>
2000-08-24 18:18:47 +0000

    harder test of XPreformatted

--- docs/userguide/ch6_tables.py
+++ docs/userguide/ch6_tables.py
@@ -282,9 +282,9 @@ text='''
    This is a non rearranging form of the <b>Paragraph</b> class;
    <b><font color=red>XML</font></b> tags are allowed in <i>text</i> and have the same
    
-   meanings as for the <b>Paragraph</b> class.
+      meanings as for the <b>Paragraph</b> class.
    As for <b>Preformatted</b>, if dedent is non zero <font color=red size=+1>dedent</font>
-   common leading spaces will be removed from the
+       common leading spaces will be removed from the
    front of each line.
 
 '''
