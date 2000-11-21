f002b8d6518cd1887b762707e459763d0b6fc128
andy_robinson <unknown>
2000-11-21 09:35:10 +0000

    Initial checkin

---
+++ reportlab/lib/tocindex.py
@@ -0,0 +1,8 @@
+# Tables of Contents and Indices
+#copyright ReportLab Inc. 2000
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/lib/tocindex.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/reportlab/lib/tocindex.py,v 1.1 2000/11/21 09:35:10 andy_robinson Exp $
+__version__=''' $Id: tocindex.py,v 1.1 2000/11/21 09:35:10 andy_robinson Exp $ '''
+__doc__=''
+
