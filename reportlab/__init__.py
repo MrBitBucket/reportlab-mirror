bf6c236d7be3adf17596ee135a396088d2571da3
rgbecker <unknown>
2000-12-11 10:16:53 +0000

    Release 1.02

--- reportlab/__init__.py
+++ reportlab/__init__.py
@@ -1,8 +1,8 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/__init__.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.7 2000/10/25 08:57:44 rgbecker Exp $
-__version__=''' $Id: __init__.py,v 1.7 2000/10/25 08:57:44 rgbecker Exp $ '''
+#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.8 2000/12/11 10:16:53 rgbecker Exp $
+__version__=''' $Id: __init__.py,v 1.8 2000/12/11 10:16:53 rgbecker Exp $ '''
 __doc__="""
 """
-Version = "1.01"
+Version = "1.02"
