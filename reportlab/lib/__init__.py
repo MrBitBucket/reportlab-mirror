a46145dc1a08281e7a5ab3d1c8686c549fa1ecfd
rgbecker <unknown>
2000-12-14 13:39:24 +0000

    Added RL_DEBUG environ flag

--- reportlab/lib/__init__.py
+++ reportlab/lib/__init__.py
@@ -2,5 +2,7 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/lib/__init__.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/lib/__init__.py,v 1.2 2000/10/25 08:57:45 rgbecker Exp $
-__version__=''' $Id: __init__.py,v 1.2 2000/10/25 08:57:45 rgbecker Exp $ '''
+#$Header: /tmp/reportlab/reportlab/lib/__init__.py,v 1.3 2000/12/14 13:39:24 rgbecker Exp $
+__version__=''' $Id: __init__.py,v 1.3 2000/12/14 13:39:24 rgbecker Exp $ '''
+import os
+RL_DEBUG = os.environ.has_key('RL_DEBUG')
