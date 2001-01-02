f1a825b6975079d05353bec9201f00bb4fe2388b
johnprecedo <unknown>
2001-01-02 14:57:46 +0000

    Added 5 new themes.

--- reportlab/lib/randomtext.py
+++ reportlab/lib/randomtext.py
@@ -2,24 +2,127 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/lib/randomtext.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/lib/randomtext.py,v 1.1 2000/12/29 02:06:13 andy_robinson Exp $
-__version__=''' $Id: randomtext.py,v 1.1 2000/12/29 02:06:13 andy_robinson Exp $ '''
+#$Header: /tmp/reportlab/reportlab/lib/randomtext.py,v 1.2 2001/01/02 14:57:46 johnprecedo Exp $
+__version__=''' $Id: randomtext.py,v 1.2 2001/01/02 14:57:46 johnprecedo Exp $ '''
 import string
 ###############################################################################
 #	generates so-called 'Greek Text' for use in filling documents.
 ###############################################################################
 """
-This modules exposes a function randomText() which generates paragraphs.
+This module exposes a function randomText() which generates paragraphs.
 These can be used when testing out document templates and stylesheets.
 A number of 'themes' are provided - please contribute more!
 We need some real Greek text too.
+
+There are currently six themes provided:
+	STARTUP (words suitable for a business plan - or not as the case may be),
+	COMPUTERS (names of programming languages and operating systems etc),
+	BLAH (variations on the word 'blah'),
+	BUZZWORD (buzzword bingo),
+	STARTREK (Star Trek),
+	PRINTING (print-related terms)
 """
 
 #theme one :-)
-STARTUP = ['strategic','direction','proactive',
-    'reengineering','forecast','resources',
-    'forward-thinking','profit','growth','doubletalk','B2B','B2C'
-    'venture capital','IPO', "NASDAQ meltdown - we're all doomed!"]
+STARTUP = ['strategic', 'direction', 'proactive', 'venture capital',
+    'reengineering', 'forecast', 'resources', 'SWOT analysis',
+    'forward-thinking', 'profit', 'growth', 'doubletalk', 'B2B', 'B2C',
+    'venture capital', 'IPO', "NASDAQ meltdown - we're all doomed!"]
+
+#theme two - computery things.
+COMPUTERS = ['Python', 'Perl', 'Pascal', 'Java', 'Javascript',
+    'VB', 'Basic', 'LISP', 'Fortran', 'ADA', 'APL', 'C', 'C++',
+    'assembler', 'Larry Wall', 'Guido van Rossum', 'XML', 'HTML',
+    'cgi', 'cgi-bin', 'Amiga', 'Macintosh', 'Dell', 'Microsoft',
+    'firewall', 'server', 'Linux', 'Unix', 'MacOS', 'BeOS', 'AS/400',
+    'sendmail', 'TCP/IP', 'SMTP', 'RFC822-compliant', 'dynamic',
+    'Internet', 'A/UX', 'Amiga OS', 'BIOS', 'Boot Managers', 'CP/M',
+    'DOS', 'File System', 'FreeBSD', 'Freeware', 'GEOS', 'GNU',
+    'Hurd', 'Linux', 'Mach', 'Macintosh OS', 'Mailing Lists', 'Minix',
+    'Multics', 'NetWare', 'NextStep', 'OS/2', 'Plan 9', 'Realtime',
+    'UNIX', 'VMS', 'Windows', 'X Windows', 'Xinu', 'Security', 'Intel', 
+    'Encryption', 'PGP' , 'software', 'ActiveX', 'AppleScript', 'awk',
+    'BETA', 'COBOL', 'Delphi', 'Dylan', 'Eiffel', 'extreme programming',
+    'Forth', 'Fortran', 'functional languages', 'Guile', 'format your hard drive',
+    'Icon', 'IDL', 'Infer', 'Intercal', 'J', 'Java', 'JavaScript', 'CD-ROM',
+    'JCL', 'Lisp', '"literate programming"', 'Logo', 'MUMPS', 'C: Drive',
+    'Modula-2', 'Modula-3', 'Oberon', 'Occam', 'OpenGL', 'Parallel Languages',
+    'Pascal', 'Perl', 'PL/I', 'PostScript', 'Prolog', 'hardware', 'Blue Screen of Death',
+    'Rexx', 'RPG', 'Scheme', 'scripting languages', 'Smalltalk', 'crash!', 'disc crash',
+    'Spanner', 'SQL', 'Tcl/Tk', 'TeX', 'TOM', 'Visual', 'Visual Basic',
+    'VRML', 'Virtual Reality Modeling Language', 'difference engine', '...went into "yo-yo mode"', 
+    'Sun', 'Sun Microsystems', 'Hewlett Packard', 'output device',
+    'CPU', 'memory', 'registers', 'monitor', 'TFT display', 'plasma screen', 
+    'bug report', '"mis-feature"', '...millions of bugs!', 'pizza',
+    '"illiterate programming"''...lots of pizza!', 'pepperoni pizza',
+    'coffee', 'Jolt Cola[TM]', 'beer', 'BEER!']
+
+#theme three - 'blah' - for when you want to be subtle. :-)
+BLAH = ['Blah', 'BLAH', 'blahblah', 'blahblahblah', 'blah-blah',
+    'blah!', '"Blah Blah Blah"', 'blah-de-blah', 'blah?', 'blah!!!',
+    'blah...', 'Blah.', 'blah;', 'blah, Blah, BLAH!', 'Blah!!!']
+
+#theme four - 'buzzword bingo' time!
+BUZZWORD = ['intellectual capital', 'market segment', 'flattening',
+		'regroup', 'platform', 'client-based', 'long-term', 'proactive',
+		'quality vector', 'out of the loop', 'implement',
+		'streamline', 'cost-centered', 'phase', 'synergy',
+		'synergize', 'interactive', 'facilitate',
+		'appropriate', 'goal-setting', 'empowering', 'low-risk high-yield',
+		'peel the onion', 'goal', 'downsize', 'result-driven',
+		'conceptualize', 'multidisciplinary', 'gap analysis', 'dysfunctional',
+		'networking', 'knowledge management', 'goal-setting',
+		'mastery learning', 'communication', 'real-estate', 'quarterly',
+		'scalable', 'Total Quality Management', 'best of breed',
+		'nimble', 'monetize', 'benchmark', 'hardball',
+		'client-centered', 'vision statement', 'empowerment',
+		'lean & mean', 'credibility', 'synergistic',
+		'backward-compatible', 'hardball', 'stretch the envelope',
+		'bleeding edge', 'networking', 'motivation', 'best practice',
+		'best of breed', 'implementation', 'Total Quality Management',
+		'undefined', 'disintermediate', 'mindset', 'architect',
+		'gap analysis', 'morale', 'objective', 'projection',
+		'contribution', 'proactive', 'go the extra mile', 'dynamic',
+		'world class', 'real estate', 'quality vector', 'credibility',
+		'appropriate', 'platform', 'projection', 'mastery learning',
+		'recognition', 'quality', 'scenario', 'performance based',
+		'solutioning', 'go the extra mile', 'downsize', 'phase',
+		'networking', 'experiencing slippage', 'knowledge management',
+		'high priority', 'process', 'ethical', 'value-added', 'implement',
+		're-factoring', 're-branding', 'embracing change']
+
+#theme five - Star Trek
+STARTREK = ['Starfleet', 'Klingon', 'Romulan', 'Cardassian', 'Vulcan',
+    'Benzite', 'IKV Pagh', 'emergency transponder', 'United Federation of Planets',
+    'Bolian', "K'Vort Class Bird-of-Prey", 'USS Enterprise', 'USS Intrepid',
+    'USS Reliant', 'USS Voyager', 'Starfleet Academy', 'Captain Picard',
+    'Captain Janeway', 'Tom Paris', 'Harry Kim', 'Counsellor Troi',
+    'Lieutenant Worf', 'Lieutenant Commander Data', 'Dr. Beverly Crusher',
+    'Admiral Nakamura', 'Irumodic Syndrome', 'Devron system', 'Admiral Pressman',
+    'asteroid field', 'sensor readings', 'Binars', 'distress signal', 'shuttlecraft',
+    'cloaking device', 'shuttle bay 2', 'Dr. Pulaski', 'Lwaxana Troi', 'Pacifica',
+    'William Riker', "Chief O'Brian", 'Soyuz class science vessel', 'Wolf-359',
+    'Galaxy class vessel', 'Utopia Planitia yards', 'photon torpedo', 'Archer IV',
+    'quantum flux', 'spacedock', 'Risa', 'Deep Space Nine', 'blood wine',
+    'quantum torpedoes', 'holodeck', 'Romulan Warbird', 'Betazoid', 'turbolift', 'battle bridge',
+    'Memory Alpha', '...with a phaser!', 'Romulan ale', 'Ferrengi', 'Klingon opera',
+    'Quark', 'wormhole', 'Bajoran', 'cruiser', 'warship', 'battlecruiser', '"Intruder alert!"',
+    'scout ship', 'science vessel', '"Borg Invasion imminent!" ', '"Abandon ship!"',
+    'Red Alert!', 'warp-core breech', '"All hands abandon ship! This is not a drill!"']
+
+#theme six - print-related terms
+PRINTING = ['points', 'picas', 'leading', 'kerning', 'CMYK', 'offset litho', 
+    'type', 'font family', 'typography', 'type designer',
+    'baseline', 'white-out type', 'WOB', 'bicameral', 'bitmap',
+    'blockletter', 'bleed', 'margin', 'body', 'widow', 'orphan',
+    'cicero', 'cursive', 'letterform', 'sidehead', 'dingbat', 'leader',
+    'DPI', 'drop-cap', 'paragraph', 'En', 'Em', 'flush left', 'left justified',
+    'right justified', 'centered', 'italic', 'Latin letterform', 'ligature',
+    'uppercase', 'lowercase', 'serif', 'sans-serif', 'weight', 'type foundry',
+    'fleuron', 'folio', 'gutter', 'whitespace', 'humanist letterform', 'caption',
+    'page', 'frame', 'ragged setting', 'flush-right', 'rule', 'drop shadows',
+    'prepress', 'spot-colour', 'duotones', 'colour separations', 'four-colour printing',
+    'Pantone[TM]', 'service bureau', 'imagesetter']
 
 
 
@@ -39,6 +142,6 @@ def randomText(theme=STARTUP):
 			else:
 				word = 'blah'
 			output = output + ' ' +word
-		output = output+'.'
+		output = output+'. '
 	return output
 
