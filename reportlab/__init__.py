e11dde8787c52a612a4006b1b61f0799549b5665
johnprecedo <unknown>
2001-02-09 15:49:01 +0000

    release 1.03

--- reportlab/__init__.py
+++ reportlab/__init__.py
@@ -1,8 +1,8 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/__init__.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.8 2000/12/11 10:16:53 rgbecker Exp $
-__version__=''' $Id: __init__.py,v 1.8 2000/12/11 10:16:53 rgbecker Exp $ '''
+#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.9 2001/02/09 15:49:01 johnprecedo Exp $
+__version__=''' $Id: __init__.py,v 1.9 2001/02/09 15:49:01 johnprecedo Exp $ '''
 __doc__="""
 """
-Version = "1.02"
+Version = "1.03"
