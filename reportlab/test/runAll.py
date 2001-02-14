3d027fbf4f6937ad85d50360938cc265922074b6
dinu_gherman <unknown>
2001-02-14 09:46:13 +0000

    Initial checkin.

---
+++ reportlab/test/runAll.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+
+"""Runs all test files in all subfolders.
+"""
+
+
+import os, glob, sys
+from reportlab.test import unittest
+
+
+def subFoldersOfFolder(folder):
+    "Return a list of full paths of all subfolders."
+
+    files = os.listdir(folder)
+    files = map(lambda f:os.path.join(folder, f), files)
+    subFolders = filter(lambda f: os.path.isdir(f), files)
+
+    return subFolders
+
+    
+def removeFiles(rootFolder, ext):
+    "Remove in a directory tree all files with some extension."
+
+    subFolders = subFoldersOfFolder(rootFolder)
+    for sub in [rootFolder] + subFolders:
+        for f in glob.glob(os.path.join(sub, '*' + ext)):
+            os.remove(f)
+
+
+def suite(folder):
+    "Build a test suite of all test files found."
+    
+    allTests = unittest.TestSuite()
+
+    # Do not use subfolders, right now...   
+    # subFolders = subFoldersOfFolder(folder)
+    subFolders = [folder]
+    for sub in subFolders:
+        files = glob.glob(os.path.join(sub, 'test_*.py'))
+        modulesToTest = map(lambda f:os.path.basename(f)[:-3], files)
+        sys.path.insert(0, sub)
+        
+        for module in map(__import__, modulesToTest):
+            allTests.addTest(module.makeSuite())
+            
+        del sys.path[0]
+        
+    return allTests
+
+
+if __name__ == '__main__':
+    folder = os.path.dirname(sys.argv[0])
+    unittest.TextTestRunner().run(suite(folder)).wasSuccessful()
+    removeFiles(folder, '.pyc')
