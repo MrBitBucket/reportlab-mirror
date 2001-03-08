1601851c69eeaa372971587b7ee90e63cb072845
dinu_gherman <unknown>
2001-03-08 01:44:00 +0000

    Added a multi-page paragraph test.

--- reportlab/test/test_platypus_paragraphs.py
+++ reportlab/test/test_platypus_paragraphs.py
@@ -1,7 +1,7 @@
 """Tests for the reportlab.platypus.paragraphs module.
 """
 
-import sys
+import sys, os, tempfile
 
 from string import split, strip, join, whitespace
 from operator import truth
@@ -12,14 +12,62 @@ from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.platypus.paraparser import ParaParser
 from reportlab.platypus.flowables import Flowable
 from reportlab.lib.colors import Color
+from reportlab.lib.units import cm
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
 from reportlab.lib.utils import _className
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from copy import deepcopy
-from reportlab.lib.abag import ABag
-
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.platypus.frames import Frame
+from reportlab.platypus.doctemplate \
+     import PageTemplate, BaseDocTemplate
+from reportlab.platypus import tableofcontents0
+from reportlab.platypus.tableofcontents0 import TableOfContents0
+from reportlab.platypus.tables import TableStyle, Table
 from reportlab.platypus.paragraph import *
 from reportlab.platypus.paragraph import _getFragWords
+
+
+def myMainPageFrame(canvas, doc):
+    "The page frame used for all PDF documents."
+    
+    canvas.saveState()
+
+    canvas.rect(2.5*cm, 2.5*cm, 15*cm, 25*cm)
+    canvas.setFont('Times-Roman', 12)
+    pageNumber = canvas.getPageNumber()
+    canvas.drawString(10*cm, cm, str(pageNumber))
+
+    canvas.restoreState()
+    
+
+class MyDocTemplate(BaseDocTemplate):
+    _invalidInitArgs = ('pageTemplates',)
+    
+    def __init__(self, filename, **kw):
+        frame1 = Frame(2.5*cm, 2.5*cm, 15*cm, 25*cm, id='F1')
+        self.allowSplitting = 0
+        apply(BaseDocTemplate.__init__, (self, filename), kw)
+        template = PageTemplate('normal', [frame1], myMainPageFrame)
+        self.addPageTemplates(template)
+
+
+class ParagraphSplitTestCase(unittest.TestCase):
+    "Test multi-page splitting of paragraphs (eyeball-test)."
+    
+    def test1(self):
+        "This makes one long multi-page paragraph."
+
+        # Build story.
+        story = []
+        styleSheet = getSampleStyleSheet()
+        bt = styleSheet['BodyText']
+
+        phrase = 'This should be a paragraph spanning at least three pages. '
+        description = phrase * 250
+        story.append(Paragraph(description, bt))
+
+        doc = MyDocTemplate('test_paragraphs_splitting.pdf')
+        doc.multiBuild0(story)
 
 
 class FragmentTestCase(unittest.TestCase):
@@ -52,6 +100,7 @@ def makeSuite():
     
     suite.addTest(FragmentTestCase('test1'))
     suite.addTest(FragmentTestCase('test2'))
+    suite.addTest(ParagraphSplitTestCase('test1'))
 
     return suite
 
