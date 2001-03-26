b930054870fefc22e84f01dbbf272e35e2791c45
johnprecedo <unknown>
2001-03-26 12:04:45 +0000

    Existing chapter 8 renamed to chapter 9. Stub for new chapter 8 on graphics added.

---
+++ docs/userguide/ch8_graphics.py
@@ -0,0 +1,15 @@
+#copyright ReportLab Inc. 2001
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/docs/userguide/ch7_custom.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/docs/userguide/Attic/ch8_graphics.py,v 1.1 2001/03/26 12:04:45 johnprecedo Exp $
+from genuserguide import *
+
+heading1("Writing your own $Flowable$ Objects")
+disc("""
+
+""")
+
+heading2("")
+
+disc("""
+""")
