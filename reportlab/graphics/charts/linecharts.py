82205dde89a9ab97114d1216e3be94fbafb71fc8
dinu_gherman <unknown>
2001-03-28 13:14:09 +0000

    Added to docstrings and tidied-up a bit.

--- reportlab/graphics/charts/linecharts.py
+++ reportlab/graphics/charts/linecharts.py
@@ -51,11 +51,40 @@ def makeSmiley(x, y, size, color):
 class LineChart(Widget):
     """Line chart with multiple lines.
 
-    Variants will be provided for ...,
-    probably by running all three off a common base class."""
+    A line chart is assumed to have one category and one value axis. 
+    Despite its generic name this particular line chart class has
+    a vertical value axis and a horizontal category one. It may
+    evolve into individual horizontal and vertical variants (like
+    with the existing bar charts).
+
+    Available attributes are:
+
+        x: x-position of lower-left chart origin
+        y: y-position of lower-left chart origin
+        width: chart width
+        height: chart height
+
+        useAbsolute: disables auto-scaling of chart elements (?)
+        lineLabelNudge: distance of data labels to data points
+        lineLabels: labels associated with data values
+        lineLabelFormat: format string or callback function
+        groupSpacing: space between categories
+
+        joinedLines: enables drawing of lines
+        usedSymbol: function returning a widget placed at data points
+
+        strokeColor: color of chart lines (?)
+        fillColor: color for chart background (?)
+        defaultColors: color list, used cyclically for data series
+
+        valueAxis: value axis object
+        categoryAxis: category axis object
+        categoryNames: category names
+
+        data: chart data, a list of data series of equal length
+    """
 
     _attrMap = {
-        'debug':isNumber,
         'x':isNumber,
         'y':isNumber,
         'width':isNumber,
@@ -72,32 +101,30 @@ class LineChart(Widget):
 
         'strokeColor':isColorOrNone,
         'fillColor':isColorOrNone,
-
         'defaultColors':SequenceOf(isColor),
 
+        'valueAxis':None,
         'categoryAxis':None,
         'categoryNames':isListOfStrings,
-        'valueAxis':None,
+
         'data':None
         }
 
     def __init__(self):
-        self.debug = 0
-
         self.x = 0
         self.y = 0
         self.width = 200
         self.height = 100
 
-        # allow for a bounding rectangle
+        # Allow for a bounding rectangle.
         self.strokeColor = None
         self.fillColor = None
 
-        # named so we have less recoding for the horizontal one :-)
+        # Named so we have less recoding for the horizontal one :-)
         self.categoryAxis = XCategoryAxis()
         self.valueAxis = YValueAxis()
 
-        # this defines two series of 3 points.  Just an example.
+        # This defines two series of 3 points.  Just an example.
         self.data = [(100,110,120,130),
                      (70, 80, 80, 90)]        
         self.categoryNames = ('North','South','East','West')
@@ -106,26 +133,24 @@ class LineChart(Widget):
         # cycle to set the fill color of each series.
         self.defaultColors = [colors.red, colors.green, colors.blue]
 
-        # control bar spacing. is useAbsolute = 1 then
+        # control spacing. if useAbsolute = 1 then
         # the next parameters are in points; otherwise
         # they are 'proportions' and are normalized to
-        # fit the available space.  Half a barSpacing
-        # is allocated at the beginning and end of the
-        # chart.
+        # fit the available space.
         self.useAbsolute = 0   #- not done yet
         self.groupSpacing = 1 #5
 
         self.lineLabels = TypedPropertyCollection(Label)
         self.lineLabelFormat = None
 
-        # this says whether the origin is inside or outside
-        # the bar - +10 means put the origin ten points
-        # above the tip of the bar if value > 0, or ten
-        # points inside if bar value < 0.  This is different
+        # This says whether the origin is above or below
+        # the data point. +10 means put the origin ten points
+        # above the data point if value > 0, or ten
+        # points below if data value < 0.  This is different
         # to label dx/dy which are not dependent on the
         # sign of the data.
         self.lineLabelNudge = 10
-        # if you have multiple series, by default they butt
+        # If you have multiple series, by default they butt
         # together.
 
         # New line chart attributes.
@@ -144,11 +169,13 @@ class LineChart(Widget):
                 ]
         
         lc = LineChart()
-        lc.x = 10
+
+        lc.x = 20
         lc.y = 10
         lc.height = 85
-        lc.width = 90
+        lc.width = 170
         lc.data = data
+
         drawing.add(lc)
 
         return drawing
@@ -164,33 +191,34 @@ class LineChart(Widget):
         self._rowLength = len(self.data[0])
         
         if self.useAbsolute:
-            # bar dimensions are absolute
+            # Dimensions are absolute.
             normFactor = 1.0
         else:
-            # bar dimensions are normalized to fit.  How wide
-            # notionally is one group of bars?
+            # Dimensions are normalized to fit.
             normWidth = self.groupSpacing
             availWidth = self.categoryAxis.scale(0)[1]
             normFactor = availWidth / normWidth
         
         self._positions = []
         for rowNo in range(len(self.data)):
-            barRow = []
+            lineRow = []
             for colNo in range(len(self.data[0])):
                 datum = self.data[rowNo][colNo]
                 (groupX, groupWidth) = self.categoryAxis.scale(colNo)
                 x = groupX + (0.5 * self.groupSpacing * normFactor)
                 y = self.valueAxis.scale(0)
                 height = self.valueAxis.scale(datum) - y
-                barRow.append((x, y+height))
-            self._positions.append(barRow)
+                lineRow.append((x, y+height))
+            self._positions.append(lineRow)
         
 
     def draw(self):
+        "Draws itself."
+        
         self.valueAxis.configure(self.data)
         self.valueAxis.setPosition(self.x, self.y, self.height)
 
-        # if zero is in chart, put x axis there, otherwise
+        # If zero is in chart, put x axis there, otherwise
         # use bottom.
         xAxisCrossesAt = self.valueAxis.scale(0)
         if ((xAxisCrossesAt > self.y + self.height) or (xAxisCrossesAt < self.y)):
@@ -241,7 +269,7 @@ class LineChart(Widget):
                 symbol = self.usedSymbol(x1, y1, 5, rowColor)
                 g.add(symbol)
 
-                # Draw item (bar) labels.
+                # Draw item labels.
                 self.drawLabel(g, rowNo, colNo, x1, y1)
 
         return g
@@ -265,7 +293,7 @@ class LineChart(Widget):
 
         if labelText:
             label = self.lineLabels[(rowNo, colNo)]
-            #hack to make sure labels are outside the bar
+            # Make sure labels are some distance off the data point.
             if y > 0:
                 label.setOrigin(x, y + self.lineLabelNudge)
             else:
@@ -283,30 +311,29 @@ def sample1():
             ]
     
     lc = LineChart()
+
     lc.x = 50
     lc.y = 50
     lc.height = 125
     lc.width = 300
     lc.data = data
-
     lc.joinedLines = 1
     lc.usedSymbol = makeFilledDiamond
     lc.lineLabelFormat = '%2.0f'
+##    lc.strokeColor = colors.yellow
 
-    lc.strokeColor = colors.yellow  # visible border
-
-    lc.valueAxis.valueMin = 0
-    lc.valueAxis.valueMax = 60
-    lc.valueAxis.valueStep = 15
-    
+    catNames = string.split('Jan Feb Mar Apr May Jun Jul Aug', ' ')
+    catNames = map(lambda n:n+'-99', catNames)
+    lc.categoryAxis.categoryNames = catNames
     lc.categoryAxis.labels.boxAnchor = 'ne'
     lc.categoryAxis.labels.dx = 8
     lc.categoryAxis.labels.dy = -2
     lc.categoryAxis.labels.angle = 30
 
-    catNames = string.split('Jan Feb Mar Apr May Jun Jul Aug', ' ')
-    catNames = map(lambda n:n+'-99', catNames)
-    lc.categoryAxis.categoryNames = catNames
+    lc.valueAxis.valueMin = 0
+    lc.valueAxis.valueMax = 60
+    lc.valueAxis.valueStep = 15
+    
     drawing.add(lc)
 
     return drawing
@@ -321,30 +348,30 @@ def sample2():
             ]
     
     lc = LineChart()
+
     lc.x = 50
     lc.y = 50
     lc.height = 125
     lc.width = 300
     lc.data = data
-
     lc.joinedLines = 1
     lc.usedSymbol = makeSmiley
     lc.lineLabelFormat = '%2.0f'
+    lc.strokeColor = colors.black
+    lc.fillColor = colors.lightblue
 
-    lc.strokeColor = colors.yellow  # visible border
-
-    lc.valueAxis.valueMin = 0
-    lc.valueAxis.valueMax = 60
-    lc.valueAxis.valueStep = 15
-    
+    catNames = string.split('Jan Feb Mar Apr May Jun Jul Aug', ' ')
+    catNames = map(lambda n:n+'-99', catNames)
+    lc.categoryAxis.categoryNames = catNames
     lc.categoryAxis.labels.boxAnchor = 'ne'
     lc.categoryAxis.labels.dx = 8
     lc.categoryAxis.labels.dy = -2
     lc.categoryAxis.labels.angle = 30
 
-    catNames = string.split('Jan Feb Mar Apr May Jun Jul Aug', ' ')
-    catNames = map(lambda n:n+'-99', catNames)
-    lc.categoryAxis.categoryNames = catNames
+    lc.valueAxis.valueMin = 0
+    lc.valueAxis.valueMax = 60
+    lc.valueAxis.valueStep = 15
+    
     drawing.add(lc)
 
     return drawing
