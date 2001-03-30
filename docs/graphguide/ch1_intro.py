d7619dfe98e14ab51cd891af6e05309e09aa0d2b
dinu_gherman <unknown>
2001-03-30 11:36:46 +0000

    Retrofitted a few changes to the content.

--- docs/graphguide/ch1_intro.py
+++ docs/graphguide/ch1_intro.py
@@ -1,7 +1,7 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/docs/userguide/ch1_intro.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/docs/graphguide/ch1_intro.py,v 1.3 2001/03/29 08:52:47 dinu_gherman Exp $
+#$Header: /tmp/reportlab/docs/graphguide/ch1_intro.py,v 1.4 2001/03/30 11:36:46 dinu_gherman Exp $
 
 from gengraphguide import *
 import reportlab
@@ -61,33 +61,38 @@ well.
 ReportLab is in the process of adding such a graphics package to
 its standard distribution.
 This document is both the "design document" and the "tutorial".
-It is also an expanded version of chepter 8 of the ReportLab User
+It is also an expanded version of chapter 8 of the ReportLab User
 Guide, "Platform Independent Graphics...".
 """)
 
 
 heading2("Requirements")
 
-disc("The package should allow: ")
-
-bullet("creation of reusable shapes collections")
-bullet("support for paths, clipping, coordinate transformations")
-bullet("writing output to PDF, Postscript, bitmap and vector formats")
-bullet("consistent font model (Type 1) with identical metrics on all platforms")
-bullet("""a framework for creating, documenting and reusing graphical "widgets" """)
-
 disc("""
-This is intended as a foundation for a chart library. However, it
-should also support the creation of other kinds of custom graphical
-applications, such as UML diagrams, CAD, project plans or whatever
-people wish to use it for. Within the charting domain our aims are:
+The graphics library should support the creation of custom
+graphical applications containing charts, diagrams, drawings, plans,
+etc. in various domains like business, finance, publishing, engineering
+and research.
+It is especially intended as a foundation for a chart library that
+happens to be the first major subpackage for a real-world client
+in the financial industry.
 """)
 
-bullet("horizontal and vertical bar charts")
-bullet("pie charts")
-bullet("horizontal and vertical line charts based on 'category' and 'value' axis")
-bullet("""time series charts based on genuine 'x' and 'y' values;
-       special knowledge of time series""")
+disc("The general graphics package should help with the following activities: ")
+
+bullet("creating reusable shapes collections")
+bullet("supporting paths, clipping and coordinate transformations")
+bullet("writing output to PDF, Postscript, bitmap and vector formats")
+bullet("using a consistent font model (Type 1)")
+bullet("providing identical metrics on all platforms")
+bullet("""using a framework for creating, documenting and reusing graphical "widgets" """)
+
+disc("""Within the charting domain the target features are:""")
+
+bullet("Horizontal/vertical bar charts based on category/value axes.")
+bullet("Horizontal/vertical line charts based on category/value axes.")
+bullet("""Special time series charts based on genuine x/y values.""")
+bullet("Simple pie charts.")
 bullet("""Compounding - one can define 'multiples' placing several charts on one 
        drawing, or arbitrary decorations around the chart. This technique 
        also allows easy overlaying of lines on bars, or of different axes on 
@@ -95,10 +100,9 @@ bullet("""Compounding - one can define 'multiples' placing several charts on one
 bullet("""'Plug-In Architecture' - with training, you can write a new chart type
        based on an existing one but only changing/adding the features you
        need to.""")
-bullet("Control over drawing size")
-bullet("Control over plot rectangle size and position within drawing")
-bullet("""Control over width, dash style, line cap style, line join style and
-       color for all lines""")
+bullet("Control over drawing size, plot rectangle size and position within drawing.")
+bullet("""Control over width, dash style, line cap/join style and
+       color for all lines.""")
 bullet("""Choice of any solid color (or gray level, or transparent) for any
        enclosed area. Fill patterns may be added later. The public library
        will be limited to RGB and possibly plain CMYK colors but we can
@@ -106,10 +110,10 @@ bullet("""Choice of any solid color (or gray level, or transparent) for any
 bullet("""Control over font name (any Type 1 font on the system) and size, plus
        the ability to scale, stretch and rotate the text
        right, left and centre alignment of label text strings with correct
-       metrics""")
+       metrics.""")
 
-disc(
-"""The charting requirements are based on a commercial sponsor in the
-financial industry who needs to create batches of charts rapidly with
-precise control over layout.
+disc("""
+The charting requirements are based on a commercial sponsor who
+needs to create batches of charts rapidly with precise control over
+layout.
 """)
