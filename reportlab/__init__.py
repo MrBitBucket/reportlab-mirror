54530767c2e326cd4db22d69e0767d280e5108cb
rgbecker <unknown>
2001-04-16 11:33:02 +0000

    Changed copyright dates

--- reportlab/__init__.py
+++ reportlab/__init__.py
@@ -1,8 +1,8 @@
-#copyright ReportLab Inc. 2000
+#copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/__init__.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.12 2001/03/30 23:09:53 rgbecker Exp $
-__version__=''' $Id: __init__.py,v 1.12 2001/03/30 23:09:53 rgbecker Exp $ '''
+#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.13 2001/04/16 11:33:02 rgbecker Exp $
+__version__=''' $Id: __init__.py,v 1.13 2001/04/16 11:33:02 rgbecker Exp $ '''
 __doc__="""The Reportlab PDF generation library."""
 Version = "1.06"
 
