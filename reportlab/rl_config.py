97a80816c184b7cb6982094ebce54fb3e6f78052
rgbecker <unknown>
2001-04-17 18:28:54 +0000

    Fix typo in T1SearchPath name

--- reportlab/rl_config.py
+++ reportlab/rl_config.py
@@ -1,7 +1,7 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/rl_config.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/rl_config.py,v 1.3 2001/04/05 09:30:11 rgbecker Exp $
+#$Header: /tmp/reportlab/reportlab/rl_config.py,v 1.4 2001/04/17 18:28:54 rgbecker Exp $
 import sys
 from reportlab.lib import pagesizes
 shapeChecking = 1
@@ -12,6 +12,6 @@ defaultImageCaching = 1					#set to zero to remove those annoying cached images
 
 #places to search for Type 1 Font files
 if sys.platform=='win32':
-	T1SearchPathPath=['c:\\Program Files\\Adobe\\Acrobat 4.0\\Resource\\Font']
+	T1SearchPath=['c:\\Program Files\\Adobe\\Acrobat 4.0\\Resource\\Font']
 else:
-	T1SearchPathPath=[]
+	T1SearchPath=[]
