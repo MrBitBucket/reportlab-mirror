aeac71ce776d63d8021850a8bdc5bd1f9662a8be
dinu_gherman <unknown>
2001-05-21 06:38:03 +0000

    Substantial rewrite.

--- reportlab/test/test_docstrings.py
+++ reportlab/test/test_docstrings.py
@@ -2,7 +2,7 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/test/test_docstrings.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/test/test_docstrings.py,v 1.6 2001/05/18 18:40:37 dinu_gherman Exp $
+#$Header: /tmp/reportlab/reportlab/test/test_docstrings.py,v 1.7 2001/05/21 06:38:03 dinu_gherman Exp $
 
 """This is a test on a package level that find all modules,
 classes, methods and functions that do not have a doc string
@@ -12,81 +12,94 @@ Currently, methods with leading and trailing double underscores
 are skipped.
 """
 
-import os, sys, glob, string, types, re
+import os, sys, glob, string, re
+from types import ModuleType, ClassType, MethodType, FunctionType
 
 import reportlab
 from reportlab.test import unittest
-from reportlab.test.utils import SecureTestCase
+from reportlab.test.utils import SecureTestCase, GlobDirectoryWalker
 
 
 RL_HOME = os.path.dirname(reportlab.__file__)
 
 
-# Should replace with test.utils.GlobDirectoryWalker, soon...
-def subFoldersOfFolder(folder):
-    "Return a list of full paths of all subfolders."
-
-    files = os.listdir(folder)
-    for i in range(len(files)):
-        files[i] = os.path.join(folder, files[i])
-    subFolders = filter(lambda f: os.path.isdir(f), files)
-
-    return subFolders
-
-
 def getModuleObjects(folder, rootName, typ):
-    "Get a list of all function objects defined *somewhere* in a package."
+    "Get a list of all objects defined *somewhere* in a package."
 
-    folders = [folder] + subFoldersOfFolder(folder)
-
+    # Define some abbreviations.
+    find = string.find
+    split = string.split
+    replace = string.replace
+    
     objects = []
-    for f in folders:
-        opath = sys.path
-        sys.path.insert(0, f)
-        cwd = os.getcwd()
-        os.chdir(f)
+    lookup = {}
+    for file in GlobDirectoryWalker(folder, '*.py'):
+        folder = os.path.dirname(file)
 
-        if not os.path.isfile('__init__.py'):
+        if os.path.basename(file) == '__init__.py':
             continue
 
-        pattern = os.path.join('*.py')
-        prefix = f[string.find(f, rootName):]
-        prefix = string.replace(prefix, os.sep, '.')
-        modNames = glob.glob(pattern)
-        modNames = filter(lambda m:string.find(m, '__init__.py') == -1, modNames)
-        # Make module names fully qualified.
-        for i in range(len(modNames)):
-            modNames[i] = prefix + '.' + modNames[i][:string.find(modNames[i], '.')]
-        for mName in modNames:
+##        if os.path.exists(os.path.join(folder, '__init__.py')):
+####            print 'skipping', os.path.join(folder, '__init__.py')
+##            continue
+
+        sys.path.insert(0, folder)
+        cwd = os.getcwd()
+        os.chdir(folder)
+        
+        modName = os.path.splitext(os.path.basename(file))[0]
+        prefix = folder[find(folder, rootName):]
+        prefix = replace(prefix, os.sep, '.')
+        mName = prefix + '.' + modName
+
+        try:
             module = __import__(mName)
-            # Get the 'real' (leaf) module
-            # (__import__ loads only the top-level one).
-            if string.find(mName, '.') != -1:
-                for part in string.split(mName, '.')[1:]:
-                    module = getattr(module, part)
+        except ImportError:
+            # Restore sys.path and working directory.        
+            os.chdir(cwd)
+            del sys.path[0]
+            continue
+
+        # Get the 'real' (leaf) module
+        # (__import__ loads only the top-level one).
+        if find(mName, '.') != -1:
+            for part in split(mName, '.')[1:]:
+                module = getattr(module, part)
+
             # Find the objects in the module's content.
             modContentNames = dir(module)
+
             # Handle modules.
-            if typ  == types.ModuleType:
-                if string.find(module.__name__, 'reportlab') > -1:
+            if typ == ModuleType:
+                if find(module.__name__, 'reportlab') > -1:
                     objects.append((mName, module))
                     continue
+
             for n in modContentNames:
                 obj = eval(mName + '.' + n)
                 # Handle functions and classes.
-                if typ in (types.FunctionType, types.ClassType):
-                    if type(obj) == typ:
+                if typ in (FunctionType, ClassType):
+                    if type(obj) == typ and not lookup.has_key(obj):
+                        if typ == ClassType:
+                            if find(obj.__module__, rootName) != 0:
+                                continue
                         objects.append((mName, obj))
+                        lookup[obj] = 1
                 # Handle methods.
-                elif typ == types.MethodType:
-                    if type(obj) == types.ClassType:
+                elif typ == MethodType:
+                    if type(obj) == ClassType:
                         for m in dir(obj):
                             a = getattr(obj, m)
-                            if type(a) == typ:
+                            if type(a) == typ and not lookup.has_key(a):
+                                if find(a.im_class.__module__, rootName) != 0:
+                                    continue
                                 cName = obj.__name__
-                                objects.append(("%s.%s" % (mName, cName), a))
+                                objects.append((mName, a))
+                                lookup[a] = 1
+
+        # Restore sys.path and working directory.        
         os.chdir(cwd)
-        sys.path = opath
+        del sys.path[0]
 
     return objects
 
@@ -98,55 +111,69 @@ class DocstringTestCase(SecureTestCase):
         "Write log file for different kind of documentable objects."
 
         cwd = os.getcwd()
-
         objects = getModuleObjects(RL_HOME, 'reportlab', objType)
-
-        expl = {types.FunctionType:'functions',
-                types.ClassType:'classes',
-                types.MethodType:'methods',
-                types.ModuleType:'modules'}[objType]
-
+        objects.sort()
         os.chdir(cwd)
+
+        expl = {FunctionType:'functions',
+                ClassType:'classes',
+                MethodType:'methods',
+                ModuleType:'modules'}[objType]
+
         path = "test_docstrings-%s.log" % expl
         file = open(path, 'w')
         file.write('No doc strings found for the following %s below.\n\n' % expl)
         p = re.compile('__.+__')
+
+        lines = []
         for name, obj in objects:
-            n = string.split(obj.__name__, '.')[-1]
-            # Skip names with leading and trailing double underscores.
-            if p.match(n):
-                continue
-            if objType != types.ModuleType:
+            if objType == MethodType:
+                n = obj.__name__
+                # Skip names with leading and trailing double underscores.
+                if p.match(n):
+                    continue
+            
+            if objType == FunctionType:
                 if not obj.__doc__ or len(obj.__doc__) == 0:
-                    file.write("%s.%s()\n" % (name, obj.__name__))
+                    lines.append("%s.%s\n" % (name, obj.__name__))
             else:
                 if not obj.__doc__ or len(obj.__doc__) == 0:
-                    file.write("%s\n" % (obj.__name__))
+                    if objType == ClassType:
+                        lines.append("%s.%s\n" % (obj.__module__, obj.__name__))
+                    elif objType == MethodType:
+                        lines.append("%s.%s\n" % (obj.im_class, obj.__name__))
+                    else:
+                        lines.append("%s\n" % (obj.__name__))
+
+        lines.sort()
+        for line in lines:
+            file.write(line)
+
         file.close()
 
 
     def test1(self):
         "Test if functions have a doc string."
 
-        self._writeLogFile(types.FunctionType)
+        self._writeLogFile(FunctionType)
 
 
     def test2(self):
         "Test if classes have a doc string."
 
-        self._writeLogFile(types.ClassType)
+        self._writeLogFile(ClassType)
 
 
     def test3(self):
         "Test if methods have a doc string."
 
-        self._writeLogFile(types.MethodType)
+        self._writeLogFile(MethodType)
 
 
     def test4(self):
         "Test if modules have a doc string."
 
-        self._writeLogFile(types.ModuleType)
+        self._writeLogFile(ModuleType)
 
 
 def makeSuite():
