a2a33cd319c16841f774294b97f6a2bcd82f3dd6
rgbecker <unknown>
2001-09-05 11:47:21 +0000

    Added some extra length type tests

--- reportlab/test/test_pdfbase_pdfutils.py
+++ reportlab/test/test_pdfbase_pdfutils.py
@@ -1,7 +1,7 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/test/test_pdfbase_pdfutils.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/test/test_pdfbase_pdfutils.py,v 1.3 2001/04/05 09:30:12 rgbecker Exp $
+#$Header: /tmp/reportlab/reportlab/test/test_pdfbase_pdfutils.py,v 1.4 2001/09/05 11:47:21 rgbecker Exp $
 """Tests for utility functions in reportlab.pdfbase.pdfutils.
 """
 
@@ -30,12 +30,15 @@ class PdfEncodingTestCase(unittest.TestCase):
     def testAsciiBase85(self):
         "Test if the obvious test for whether ASCII-Base85 encoding works."
 
-        plainText = 'What is the average velocity of a sparrow?'
-        encoded = _AsciiBase85Encode(plainText)
-        decoded = _AsciiBase85Decode(encoded)
-        
         msg = "Round-trip AsciiBase85 encoding failed."
-        assert decoded == plainText, msg
+        plain = 'What is the average velocity of a sparrow?'
+
+        #the remainder block can be absent or from 1 to 4 bytes
+        for i in xrange(55):
+            encoded = _AsciiBase85Encode(plain)
+            decoded = _AsciiBase85Decode(encoded)
+            assert decoded == plain, msg
+            plain = plain + chr(i)
 
 
 def makeSuite():
