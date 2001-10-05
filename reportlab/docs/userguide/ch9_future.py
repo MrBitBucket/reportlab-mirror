ecaeeffaba535907a20cad8ef0a990308cf3352a
rgbecker <unknown>
2001-10-05 12:33:33 +0000

    Moved from original project docs, history lost

---
+++ reportlab/docs/userguide/ch9_future.py
@@ -0,0 +1,36 @@
+#copyright ReportLab Inc. 2000
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/docs/userguide/ch9_future.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/reportlab/docs/userguide/ch9_future.py,v 1.1 2001/10/05 12:33:33 rgbecker Exp $
+from rl_doc_utils import *
+
+heading1("Future Directions")
+
+disc("""We have a very long list of things we plan to do
+and what we do first will most likely be inspired by customer
+or user interest.
+""")
+
+disc("""
+We plan to provide a large number of pre-designed Platypus example
+document types -- brochure, newsletter, business letter, thesis, memo,
+etcetera, to give our users a better boost towards the solutions they
+desire.
+""")
+
+disc("""
+We plan to fully support adding fonts and internationalization, which are
+not well supported in the current release.""")
+
+disc("""
+We plan to fully support some of the more obscure features of PDF
+such as general hyperlinks, which are not yet well supported.
+""")
+
+disc("""
+We are also open for suggestions.  Please let us know what you think
+is missing.  You can also offer patches or contributions.  Please
+look to $http://www.reportlab.com$ for the latest mailing list and
+contact information.""")
+
+# this comment is a trivial test of SF checkin rights - delete it some time!  AR 2001-04-17
---
+++ reportlab/docs/userguide/genuserguide.py
@@ -0,0 +1,72 @@
+#!/bin/env python
+#copyright ReportLab Inc. 2001
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/docs/userguide/genuserguide.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/reportlab/docs/userguide/genuserguide.py,v 1.1 2001/10/05 12:33:33 rgbecker Exp $
+
+__version__=''' $Id: genuserguide.py,v 1.1 2001/10/05 12:33:33 rgbecker Exp $ '''
+
+__doc__ = """
+This module contains the script for building the user guide.
+"""
+
+import os, sys
+sys.path.insert(0,os.path.join('..','tools'))
+from rl_doc_utils import *
+
+def run(pagesize):
+
+	doc = RLDocTemplate('userguide.pdf',pagesize = pagesize)
+
+	#this builds the story
+	#resetStory()
+
+	import ch1_intro
+	import ch2_graphics
+	import ch3_pdffeatures
+	import ch4_platypus_concepts
+	import ch5_paragraphs
+	import ch6_tables
+	import ch7_custom
+	import ch9_future
+
+	import app_demos
+
+	story = getStory()
+	print 'Built story contains %d flowables...' % len(story)
+	doc.build(story)
+	print 'Saved userguide.pdf'
+
+	# copy to target
+	import reportlab
+	docdir = os.path.dirname(reportlab.__file__) + os.sep + 'docs'
+	destfn = docdir + os.sep + 'userguide.pdf'
+	import shutil
+	shutil.copyfile('userguide.pdf',
+					destfn)
+	print 'copied to %s' % destfn
+	
+	# remove *.pyc files
+	pat = os.path.join(os.path.dirname(sys.argv[0]), '*.pyc')
+	for file in glob.glob(pat):
+		os.remove(file)
+
+
+def makeSuite():
+    "standard test harness support - run self as separate process"
+    from reportlab.test.utils import ScriptThatMakesFileTest
+    return ScriptThatMakesFileTest('../docs/userguide',
+								   'genuserguide.py',
+								   'userguide.pdf')
+
+if __name__=="__main__":
+	if len(sys.argv) > 1:
+		try:
+			(w, h) = eval(sys.argv[1])
+		except:
+			print 'Expected page size in argument 1', sys.argv[1]
+			raise
+		print 'set page size to',sys.argv[1]
+	else:
+		(w, h) = defaultPageSize
+	run((w, h))
