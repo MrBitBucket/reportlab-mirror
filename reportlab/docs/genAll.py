cd18d816e986c6db7537dd21b20e1876255d2471
andy_robinson <unknown>
2001-10-28 19:58:32 +0000

    Whoops, forgot it yesterday.

---
+++ reportlab/docs/genAll.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+"""Runs the three manual-building scripts"""
+if __name__=='__main__':
+	import os, sys
+	d = os.path.dirname(sys.argv[0])
+	if not d: d = '.'
+	if not os.path.isabs(d):
+		d = os.path.normpath(os.path.join(os.getcwd(),d))
+	for p in ('reference/genreference.py', 'userguide/genuserguide.py',
+			'graphguide/gengraphguide.py'):
+		os.chdir(d)
+		os.chdir(os.path.dirname(p))
+		os.system('%s %s' % (sys.executable,os.path.basename(p)))
