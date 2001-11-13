c65041c28cf881da9cf94e36d8f4ba0068a6b876
rgbecker <unknown>
2001-11-13 14:28:38 +0000

    Added linux2 following email from Niki Spahiev

--- rl_addons/renderPM/setup.py
+++ rl_addons/renderPM/setup.py
@@ -18,7 +18,7 @@ if __name__=='__main__': #NO RUNTESTS
 	LIBART_SRCS=glob(pJoin(LIBART_DIR, 'art_*.c'))
 	GT1_DIR=pJoin(DEVEL_DIR,'gt1')
 	GLIB_DIR=pJoin(DEVEL_DIR,'glib')
-	if sys.platform in ['win32', 'sunos5', 'freebsd4', 'mac',]:
+	if sys.platform in ['win32', 'sunos5', 'freebsd4', 'mac', 'linux2',]:
 		LIBS=[]
 	else:
 		raise ValueError, "Don't know about other systems"
