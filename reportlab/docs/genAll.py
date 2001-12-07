523413baf4b96d7c56be1ba511fe9698671a7ebc
rgbecker <unknown>
2001-12-07 11:26:47 +0000

    Allow for use in daily.py

--- reportlab/docs/genAll.py
+++ reportlab/docs/genAll.py
@@ -1,22 +1,23 @@
 #!/bin/env python
-"""Runs the three manual-building scripts"""
-if __name__=='__main__':
-    import os, sys
-    d = os.path.dirname(sys.argv[0])
+import os
+def _genAll(d=None,quiet=''):
+	if not d: d = '.'
+	if not os.path.isabs(d):
+		d = os.path.normpath(os.path.join(os.getcwd(),d))
+	for p in ('reference/genreference.py',
+			  'userguide/genuserguide.py',
+			  'graphguide/gengraphguide.py',
+			  '../tools/docco/graphdocpy.py'):
+		os.chdir(d)
+		os.chdir(os.path.dirname(p))
+		os.system('%s %s %s' % (sys.executable,os.path.basename(p), quiet))
 
-    #need a quiet mode for the test suite   
-    if '-s' in sys.argv:  # 'silent
-        quiet = '-s'
-    else:
-        quiet = ''
-        
-    if not d: d = '.'
-    if not os.path.isabs(d):
-        d = os.path.normpath(os.path.join(os.getcwd(),d))
-    for p in ('reference/genreference.py',
-              'userguide/genuserguide.py',
-              'graphguide/gengraphguide.py',
-              '../tools/docco/graphdocpy.py'):
-        os.chdir(d)
-        os.chdir(os.path.dirname(p))
-        os.system('%s %s %s' % (sys.executable,os.path.basename(p), quiet))
+"""Runs the manual-building scripts"""
+if __name__=='__main__':
+	import sys
+	#need a quiet mode for the test suite	
+	if '-s' in sys.argv:  # 'silent
+		quiet = '-s'
+	else:
+		quiet = ''
+	_genAll(os.path.dirname(sys.argv[0]),quiet)
