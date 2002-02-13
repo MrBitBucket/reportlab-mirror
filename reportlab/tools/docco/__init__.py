4a02a94eeb6f4237d94c9650b220d5274ef1dcd4
rgbecker <unknown>
2002-02-13 17:46:40 +0000

    Ensure non-zero length

---
+++ reportlab/tools/docco/__init__.py
@@ -0,0 +1,4 @@
+#copyright ReportLab Inc. 2000-2002
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/tools/docco/__init__.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/reportlab/tools/docco/__init__.py,v 1.2 2002/02/13 17:46:40 rgbecker Exp $
---
+++ reportlab/tools/pythonpoint/__init__.py
@@ -0,0 +1,5 @@
+#copyright ReportLab Inc. 2000-2002
+#see license.txt for license details
+#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/tools/pythonpoint/__init__.py?cvsroot=reportlab
+#$Header: /tmp/reportlab/reportlab/tools/pythonpoint/__init__.py,v 1.2 2002/02/13 17:46:40 rgbecker Exp $
+
