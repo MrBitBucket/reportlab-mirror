1279defcc386a4723ce24104e4cad7acb7b38110
rgbecker <unknown>
2002-03-21 18:10:18 +0000

    Added DebugMemo stuff

--- reportlab/lib/utils.py
+++ reportlab/lib/utils.py
@@ -1,8 +1,8 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/lib/utils.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/lib/utils.py,v 1.25 2002/03/18 14:23:31 rgbecker Exp $
-__version__=''' $Id: utils.py,v 1.25 2002/03/18 14:23:31 rgbecker Exp $ '''
+#$Header: /tmp/reportlab/reportlab/lib/utils.py,v 1.26 2002/03/21 18:10:18 rgbecker Exp $
+__version__=''' $Id: utils.py,v 1.26 2002/03/21 18:10:18 rgbecker Exp $ '''
 
 import string, os, sys
 from types import *
@@ -36,13 +36,13 @@ if ',' in fp_str(0.25):
 	def fp_str(*a):
 		return string.replace(apply(_FP_STR,a),',','.')
 
-def recursiveImport(modulename, baseDir=None):
+def recursiveImport(modulename, baseDir=None, noCWD=0):
 	"""Dynamically imports possible packagized module, or raises ImportError"""
 	import imp
 	parts = string.split(modulename, '.')
 	part = parts[0]
 	path = list(baseDir and (type(baseDir) not in SeqTypes and [baseDir] or filter(None,baseDir)) or None)
-	if '.' not in path: path.insert(0,'.')
+	if not noCWD and '.' not in path: path.insert(0,'.')
 
 	#make import errors a bit more informative
 	try:
@@ -155,3 +155,117 @@ def _className(self):
 		return name
 	except AttributeError:
 		return str(self)
+
+class DebugMemo:
+	'''Intended as a simple report back encapsulator'''
+	def __init__(self,fn='rl_dbgpkg.pickle',mode='w',getScript=1,modules=(),**kw):
+		import time, socket
+		self.fn = fn
+		if mode!='w': return
+		self.store = store = {}
+		if sys.exc_info() != (None,None,None):
+			import StringIO, traceback
+			s = StringIO.StringIO()
+			traceback.print_exc(None,s)
+			store['__traceback'] = s.getvalue()
+		cwd=os.getcwd()
+		lcwd = os.listdir(cwd)
+		store.update({	'gmt': time.asctime(time.gmtime(time.time())),
+						'platform': sys.platform,
+						'version': sys.version,
+						'executable': sys.executable,
+						'prefix': sys.prefix,
+						'path': sys.path,
+						'argv': sys.argv,
+						'cwd': cwd,
+						'hostname': socket.gethostname(),
+						'listdir': lcwd,
+						})
+		if hasattr(os,'uname'):
+			store.update({
+				'uname': os.uname(),
+				'ctermid': os.ctermid(),
+				'getgid': os.getgid(),
+				'getuid': os.getuid(),
+				'getegid': os.getegid(),
+				'geteuid': os.geteuid(),
+				'getlogin': os.getlogin(),
+				'getgroups': os.getgroups(),
+				'getpgrp': os.getpgrp(),
+				'getpid': os.getpid(),
+				'getppid': os.getppid(),
+				})
+		if getScript:
+			fn = os.path.abspath(sys.argv[0])
+			if os.path.isfile(fn):
+				store['__script'] = open(fn,'r').read()
+		module_versions = {}
+		for n,m in sys.modules.items():
+			if n=='reportlab' or n=='rlextra' or n[:10]=='reportlab.' or n[:8]=='rlextra.':
+				v = getattr(m,'__version__',None)
+				if v: module_versions[n] = v
+		store['__module_versions'] = module_versions
+		self.store['__payload'] = {}
+		self._add(kw)
+
+	def _add(self,D):
+		payload = self.store['__payload']
+		for k, v in D.items():
+			payload[k] = v
+
+	def add(self,**kw):
+		self._add(kw)
+
+	def dump(self):
+		import pickle
+		pickle.dump(self.store,open(self.fn,'wb'))
+
+	def load(self):
+		import pickle
+		self.store = pickle.load(open(self.fn,'rb'))
+
+	def _show_module_versions(self,k,v):
+		print k[2:]
+		K = v.keys()
+		K.sort()
+		for k in K:
+			vk = v[k]
+			try:
+				m = recursiveImport(k,sys.path[:],1)
+				d = getattr(m,'__version__',None)==vk and 'SAME' or 'DIFFERENT'
+			except:
+				m = None
+				d = '??????unknown??????'
+			print '  %s = %s (%s)' % (k,vk,d)
+
+	def _banner(self,k,what):
+		print '###################%s %s##################' % (what,k[2:])
+
+	def _start(self,k):
+		self._banner(k,'Start  ')
+
+	def _finish(self,k):
+		self._banner(k,'Finish ')
+
+	def _show_lines(self,k,v):
+		self._start(k)
+		print v
+		self._finish(k)
+
+	def _show_payload(self,k,v):
+		if v:
+			import pprint
+			self._start(k)
+			pprint.pprint(v)
+			self._finish(k)
+
+	specials = {'__module_versions': _show_module_versions,
+				'__payload': _show_payload,
+				'__traceback': _show_lines,
+				'__script': _show_lines,
+				}
+	def show(self):
+		for k, v in self.store.items():
+			if k not in self.specials.keys(): print '%-15s = %s' % (k,v)
+		for k, v in self.store.items():
+			if k in self.specials.keys(): apply(self.specials[k],(self,k,v))
