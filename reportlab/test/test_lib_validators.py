85245dc078d6fbc35a24a661a4380a7035f754a7
dinu_gherman <unknown>
2002-05-24 09:54:17 +0000

    'Typed' arguments to % operator, removes Jython complaints.

--- reportlab/test/test_lib_validators.py
+++ reportlab/test/test_lib_validators.py
@@ -1,8 +1,10 @@
 """Tests (incomplete) for the reportlab.lib.validators module.
 """
+
 from reportlab.test import unittest
 from reportlab.lib import colors
 from reportlab.lib import validators
+
 
 class ValidatorTestCase(unittest.TestCase):
 	"Test validating functions."
@@ -10,15 +12,16 @@ class ValidatorTestCase(unittest.TestCase):
 	def test0(self):
 		"Test isBoolean validator."
 
-		msg = 'isBoolean Validation failed %s!'
+		msg = "Validation failed for 'boolean' %s!"
 
 		booleans = [0, 1, 'yes','no','true','false']
 		badbooleans = ['a',3,-1,()]
 		isBoolean = validators.isBoolean
 		for b in booleans:
-			assert isBoolean(b) == 1, msg % b
+			assert isBoolean(b) == 1, msg % str(b)
 		for b in badbooleans:
-			assert isBoolean(b) == 0, msg % b
+			assert isBoolean(b) == 0, msg % str(b)
+
 
 	def test1(self):
 		"Test isNumber validator."
@@ -33,9 +36,13 @@ class ValidatorTestCase(unittest.TestCase):
 			assert isNumber(n) == 1, msg % str(n)
 		for n in badNumbers:
 			assert isNumber(n) == 0, msg % str(n)
+
+		msg = 'Validation failed for numbers %s!'
+
 		assert isListOfNumbers(numbers) == 1, msg % str(numbers)
 		assert isListOfNumbers(badNumbers) == 0, msg % str(badNumbers)
 		assert isListOfNumbers(numbers+[colors]) == 0, msg % str(numbers+[colors])
+
 
 	def test2(self):
 		"Test isNumberOrNone validator."
@@ -58,12 +65,15 @@ class ValidatorTestCase(unittest.TestCase):
 		isString = validators.isString
 		isListOfStrings = validators.isListOfStrings
 		for s in strings:
-			assert isString(s) == 1, msg % s
+			assert isString(s) == 1, msg % str(s)
 		for s in badStrings:
-			assert isString(s) == 0, msg % s
-		assert isListOfStrings(strings) == 1, msg % strings
-		assert isListOfStrings(badStrings) == 0, msg % badStrings
-		assert isListOfStrings(strings+[1]) == 0, msg % strings+[1]
+			assert isString(s) == 0, msg % str(s)
+
+		msg = 'Validation failed for strings %s!'
+
+		assert isListOfStrings(strings) == 1, msg % str(strings)
+		assert isListOfStrings(badStrings) == 0, msg % str(badStrings)
+		assert isListOfStrings(strings+[1]) == 0, msg % str(strings+[1])
 
 
 	def test5(self):
@@ -114,28 +124,35 @@ class ValidatorTestCase(unittest.TestCase):
 		except ValueError:
 			pass
 
+
 	def test7(self):
 		"Test isInt validator"
+
 		msg = 'Validation failed for isInt %s!'
+
 		isInt = validators.isInt
 		for c in (1,2,-3,0,'-4','4'):
-			assert isInt(c), msg%c
+			assert isInt(c), msg % str(c)
 
 		for c in (1.2,0.0,-3.0,'-4.0','4.4','AAAA'):
-			assert not isInt(c), msg%c
+			assert not isInt(c), msg % str(c)
+
 
 	def test8(self):
 		"test Sequence of validator"
+
 		msg = 'Validation failed for SequenceOf %s!'
+
 		v=validators.SequenceOf(validators.OneOf(('eps','pdf','png','gif','jpg','tif')),lo=1,hi=3,emptyOK=0)
 		for c in (['png'],('eps',),('eps','pdf')):
-			assert v(c), msg%c
+			assert v(c), msg % str(c)
 		v._lo = 2
 		for c in ([],(),('eps'),('eps','pdf','a'),['eps','pdf','png','gif']):
-			assert not v(c), msg%c
+			assert not v(c), msg % str(c)
 		v._emptyOK=1
 		for c in ([],(),('eps','pdf')):
-			assert v(c), msg%c
+			assert v(c), msg % str(c)
+
 
 def makeSuite():
 	suite = unittest.TestSuite()
