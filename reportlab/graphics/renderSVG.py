b5348b79b8c9b416c030c80dccadb3c3729cee39
dinu_gherman <unknown>
2002-06-03 10:43:44 +0000

    Added newline before svg document element.

--- reportlab/graphics/renderSVG.py
+++ reportlab/graphics/renderSVG.py
@@ -140,7 +140,7 @@ class SVGCanvas:
 
         file.write("""\
 <?xml version="1.0" encoding="iso-8859-1"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20000303 Stylable//EN" "http://www.w3.org/TR/2000/03/WD-SVG-20000303/DTD/svg-20000303-stylable.dtd" >""")
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20000303 Stylable//EN" "http://www.w3.org/TR/2000/03/WD-SVG-20000303/DTD/svg-20000303-stylable.dtd" >\n""")
 
         # use = self.doc.createElement('use')
         # use.setAttribute("xlink:href", "#group")
