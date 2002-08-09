c85516ef91b28da748d38b697365821e441dfd8b
rgbecker <unknown>
2002-08-09 11:54:12 +0000

    Release 1.15

--- reportlab/__init__.py
+++ reportlab/__init__.py
@@ -1,10 +1,10 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/__init__.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.22 2002/07/24 19:56:34 andy_robinson Exp $
-__version__=''' $Id: __init__.py,v 1.22 2002/07/24 19:56:34 andy_robinson Exp $ '''
+#$Header: /tmp/reportlab/reportlab/__init__.py,v 1.23 2002/08/09 11:54:12 rgbecker Exp $
+__version__=''' $Id: __init__.py,v 1.23 2002/08/09 11:54:12 rgbecker Exp $ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "1.14"
+Version = "1.15"
 
 def getStory(context):
     if context.target == 'UserGuide':
