02aa4e9d0709911578742203d90a37ad14eeaa81
rgbecker <unknown>
2002-10-19 09:58:47 +0000

    Fix for tuple case in definePath

--- reportlab/graphics/shapes.py
+++ reportlab/graphics/shapes.py
@@ -1,11 +1,11 @@
 #copyright ReportLab Inc. 2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/graphics/shapes.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/graphics/shapes.py,v 1.80 2002/08/11 09:59:57 rgbecker Exp $
+#$Header: /tmp/reportlab/reportlab/graphics/shapes.py,v 1.81 2002/10/19 09:58:47 rgbecker Exp $
 """
 core of the graphics library - defines Drawing and Shapes
 """
-__version__=''' $Id: shapes.py,v 1.80 2002/08/11 09:59:57 rgbecker Exp $ '''
+__version__=''' $Id: shapes.py,v 1.81 2002/10/19 09:58:47 rgbecker Exp $ '''
 
 import string, os, sys
 from math import pi, cos, sin, tan
@@ -425,20 +425,23 @@ class Group(Shape):
         if b and b not in C: C = [b]+C
         return C
 
-def _addObjImport(obj,I):
+def _addObjImport(obj,I,n=None):
     '''add an import of obj's class to a dictionary of imports'''
     from inspect import getmodule
     c = obj.__class__
     m = getmodule(c).__name__
-    cn = c.__name__
+    n = n or c.__name__
     if not I.has_key(m):
-        I[m] = [cn]
-    elif cn not in I[m]:
-        I[m].append(cn)
+        I[m] = [n]
+    elif n not in I[m]:
+        I[m].append(n)
 
 def _repr(self,I=None):
     '''return a repr style string with named fixed args first, then keywords'''
     if type(self) is InstanceType:
+        if self is EmptyClipPath:
+            _addObjImport(self,I,'EmptyClipPath')
+            return 'EmptyClipPath'
         if I: _addObjImport(self,I)
         if isinstance(self,Shape):
             from inspect import getargs
@@ -797,7 +800,7 @@ def definePath(pathSegs=[],isClipPath=0, dx=0, dy=0, **kw):
         if len(args)!=_PATH_OP_ARG_COUNT[op]:
             raise ValueError, '%s bad arguments %s' % (opName,str(args))
         O.append(op)
-        P.extend(args)
+        P.extend(list(args))
     for d,o in (dx,0), (dy,1):
         for i in xrange(o,len(P),2):
             P[i] = P[i]+d
