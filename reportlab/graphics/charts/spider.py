01eab010f13f5f3ae0ec7c800840e58d67ec39c6
rgbecker <unknown>
2002-12-03 13:58:35 +0000

    Fix up for labels etc

--- reportlab/graphics/charts/spider.py
+++ reportlab/graphics/charts/spider.py
@@ -1,7 +1,7 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/graphics/charts/spider.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/graphics/charts/spider.py,v 1.1 2002/12/02 23:16:07 andy_robinson Exp $
+#$Header: /tmp/reportlab/reportlab/graphics/charts/spider.py,v 1.2 2002/12/03 13:58:35 rgbecker Exp $
 # spider chart, also known as radar chart
 
 """Spider Chart
@@ -10,7 +10,7 @@ Normal use shows variation of 5-10 parameters against some 'norm' or target.
 When there is more than one series, place the series with the largest
 numbers first, as it will be overdrawn by each successive one.
 """
-__version__=''' $Id: spider.py,v 1.1 2002/12/02 23:16:07 andy_robinson Exp $ '''
+__version__=''' $Id: spider.py,v 1.2 2002/12/03 13:58:35 rgbecker Exp $ '''
 
 import copy
 from math import sin, cos, pi
@@ -27,8 +27,9 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.graphics.shapes import Group, Drawing, Line, Rect, Polygon, Ellipse, \
     Wedge, String, STATE_DEFAULTS
 from reportlab.graphics.widgetbase import Widget, TypedPropertyCollection, PropHolder
+from reportlab.graphics.charts.areas import PlotArea
 
-
+_ANGLE2LABEL=[]
 
 class StrandProperties(PropHolder):
     """This holds descriptive information about concentric 'strands'.
@@ -41,6 +42,10 @@ class StrandProperties(PropHolder):
         fillColor = AttrMapValue(isColorOrNone),
         strokeColor = AttrMapValue(isColorOrNone),
         strokeDashArray = AttrMapValue(isListOfNumbersOrNone),
+        fontName = AttrMapValue(isString),
+        fontSize = AttrMapValue(isNumber),
+        fontColor = AttrMapValue(isColorOrNone),
+        labelRadius = AttrMapValue(isNumber),
         )
 
     def __init__(self):
@@ -48,14 +53,13 @@ class StrandProperties(PropHolder):
         self.fillColor = None
         self.strokeColor = STATE_DEFAULTS["strokeColor"]
         self.strokeDashArray = STATE_DEFAULTS["strokeDashArray"]
+        self.fontName = STATE_DEFAULTS["fontName"]
+        self.fontSize = STATE_DEFAULTS["fontSize"]
+        self.fontColor = STATE_DEFAULTS["fillColor"]
+        self.labelRadius = 1.2
 
-
-class SpiderChart(Widget):
-    _attrMap = AttrMap(
-        x = AttrMapValue(isNumber, desc='X position of the chart within its container.'),
-        y = AttrMapValue(isNumber, desc='Y position of the chart within its container.'),
-        width = AttrMapValue(isNumber, desc='width of spider bounding box. Need not be same as width.'),
-        height = AttrMapValue(isNumber, desc='height of spider bounding box.  Need not be same as height.'),
+class SpiderChart(PlotArea):
+    _attrMap = AttrMap(BASE=PlotArea,
         data = AttrMapValue(None, desc='Data to be plotted, list of (lists of) numbers.'),
         labels = AttrMapValue(isListOfStringsOrNone, desc="optional list of labels to use for each data point"),
         startAngle = AttrMapValue(isNumber, desc="angle of first slice; like the compass, 0 is due North"),
@@ -64,10 +68,8 @@ class SpiderChart(Widget):
         )
 
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.width = 100
-        self.height = 100
+        PlotArea.__init__(self)
+
         self.data = [[10,12,14,16,14,12], [6,8,10,12,9,11]]
         self.labels = None  # or list of strings
         self.startAngle = 90
@@ -94,7 +96,7 @@ class SpiderChart(Widget):
 
     def normalizeData(self, outer = 0.0):
         """Turns data into normalized ones where each datum is < 1.0,
-        and 1.0 = maximum radius.  Adds 10% at outside edge by default""" 
+        and 1.0 = maximum radius.  Adds 10% at outside edge by default"""
         data = self.data
         theMax = 0.0
         for row in data:
@@ -116,17 +118,10 @@ class SpiderChart(Widget):
     def polarToRect(self, r, theta):
         "Convert to rectangular based on current size"
         return (self._centerx + r * sin(theta),self._centery + r * cos(theta))
-    
+
     def draw(self):
         # normalize slice data
-        g = Group()
-
-
-        # show bounding box
-        g.add(Rect(self.x, self.y, self.width, self.height,
-                   strokeColor=colors.red,
-                   fillColor=None))
-
+        g = self.makeBackground() or Group()
 
         xradius = self.width/2.0
         yradius = self.height/2.0
@@ -135,25 +130,35 @@ class SpiderChart(Widget):
         self._centery = centery = self.y + yradius
 
         data = self.normalizeData()
-        
+
         n = len(self.data[0])
         angleBetween = (2 * pi)/n
-        
+
         # make a list of all spoke angles
         angles = []
         a = (self.startAngle * pi / 180)
         for i in range(n):
             angles.append(a)
             a = a + angleBetween
-            
 
 
-        print '%d slices each of %0.2f radians here: %s' % (n, angleBetween, repr(angles))
-        
+
+        #print '%d slices each of %0.2f radians here: %s' % (n, angleBetween, repr(angles))
         if self.direction == "anticlockwise":
             whichWay = 1
         else:
             whichWay = -1
+
+        #labels
+        if self.labels is None:
+            labels = [''] * n
+        else:
+            labels = self.labels
+            #there's no point in raising errors for less than enough errors if
+            #we silently create all for the extreme case of no labels.
+            i = n-len(labels)
+            if i>0:
+                labels = labels + ['']*i
 
         i = 0
         startAngle = self.startAngle
@@ -165,16 +170,13 @@ class SpiderChart(Widget):
                          centery + radius * cos(angle),
                          strokeWidth = 0.5
                          )
-            print 'added spoke (%0.2f, %0.2f) -> (%0.2f, %0.2f)' % (spoke.x1,
-                                                                    spoke.y1,
-                                                                    spoke.x2,
-                                                                    spoke.y2)
-                         
+            #print 'added spoke (%0.2f, %0.2f) -> (%0.2f, %0.2f)' % (spoke.x1, spoke.y1, spoke.x2, spoke.y2)
+
             spokes.append(spoke)
 
         # now plot the polygons
 
-        rowIdx = 0    
+        rowIdx = 0
         for row in data:
             # series plot
             points = []
@@ -199,16 +201,13 @@ class SpiderChart(Widget):
                 strand.strokeDashArray = self.strands[rowIdx].strokeDashArray
 
                 g.add(strand)
-                
+
             rowIdx = rowIdx + 1
 
         # spokes go over strands
         for spoke in spokes:
             g.add(spoke)
         return g
-
-
-
 
 def sample1():
     "Make a simple spider chart"
@@ -220,9 +219,10 @@ def sample1():
     pc.y = 50
     pc.width = 300
     pc.height = 300
-    pc.data = [[10,12,14,16,14,12], [6,8,10,12,9,11]]
+    pc.data = [[10,12,14,16,14,12], [6,8,10,12,9,15],[7,8,17,4,12,8,3]]
     pc.labels = ['a','b','c','d','e','f']
-    
+    pc.strands[2].fillColor=colors.palegreen
+
     d.add(pc)
 
     return d
@@ -232,4 +232,4 @@ if __name__=='__main__':
     d = sample1()
     from reportlab.graphics.renderPDF import drawToFile
     drawToFile(d, 'spider.pdf')
-    print 'saved spider.pdf'
+    #print 'saved spider.pdf'
