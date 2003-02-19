dce35178df0ccc7c937310ed191b8cd8804675cb
rgbecker <unknown>
2003-02-19 10:24:04 +0000

    Fix wrong call, suggested by Stuart Bishop's patch

--- rl_addons/pyRXP/pyRXP.c
+++ rl_addons/pyRXP/pyRXP.c
@@ -2,9 +2,9 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/rl_addons/pyRXP/pyRXP.c?cvsroot=reportlab
-#$Header: /tmp/reportlab/rl_addons/pyRXP/pyRXP.c,v 1.10 2002/10/25 15:27:40 rgbecker Exp $
+#$Header: /tmp/reportlab/rl_addons/pyRXP/pyRXP.c,v 1.11 2003/02/19 10:24:04 rgbecker Exp $
  ****************************************************************************/
-static char* __version__=" $Id: pyRXP.c,v 1.10 2002/10/25 15:27:40 rgbecker Exp $ ";
+static char* __version__=" $Id: pyRXP.c,v 1.11 2003/02/19 10:24:04 rgbecker Exp $ ";
 #include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +24,7 @@ static char* __version__=" $Id: pyRXP.c,v 1.10 2002/10/25 15:27:40 rgbecker Exp 
 #include "stdio16.h"
 #include "version.h"
 #include "namespaces.h"
-#define VERSION "0.9"
+#define VERSION "0.91"
 #define MODULE "pyRXP"
 #define MAX_DEPTH 256
 static PyObject *moduleError;
@@ -675,7 +675,7 @@ static PyObject* pyRXPParser_getattr(pyRXPParserObject *self, char *name)
 	int	i;
 	if(!strcmp(name,"warnCB")) return _get_OB(name,self->warnCB);
 	else if(!strcmp(name,"eoCB")) return _get_OB(name,self->eoCB);
-	else if(!strcmp(name,"fourth")) return _get_OB(name,self->eoCB);
+	else if(!strcmp(name,"fourth")) return _get_OB(name,self->fourth);
 	else if(!strcmp(name,"srcName")){
 		Py_INCREF(self->srcName);
 		return self->srcName;
