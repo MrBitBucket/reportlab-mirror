4bdea95f809b5c1393dbac4a6274c49795ed40c8
rgbecker <unknown>
2003-03-27 19:30:42 +0000

    Attempt to make this semi-automatic

--- rl_addons/renderPM/libart_lgpl/config.h
+++ rl_addons/renderPM/libart_lgpl/config.h
@@ -3,11 +3,14 @@
 
 /* Define if your processor stores words with the most significant
    byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+#ifdef macintosh
+#	define WORDS_BIGENDIAN
+#else
 /* #undef WORDS_BIGENDIAN */
+#endif
 
 /* Name of package */
 #define PACKAGE "libart_lgpl"
 
 /* Version number of package */
 #define VERSION "2.3.10"
-
