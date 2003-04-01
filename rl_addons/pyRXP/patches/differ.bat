b4c31dba75f0458af46eb5e2cce0c67507ac5cad
rgbecker <unknown>
2003-04-01 08:03:56 +0000

    Improved discrimination

--- rl_addons/pyRXP/patches/differ.bat
+++ rl_addons/pyRXP/patches/differ.bat
@@ -1,3 +1,3 @@
 @echo off
 rem command used to get patch differences
-diff -rc -I"Copyright (c) 1997" -I"$Header: /tmp/reportlab/rl_addons/pyRXP/patches/differ.bat,v 1.1 2002/03/22 11:07:52 rgbecker Exp $Id:" %1 %2 > %3
+diff -rc -I"Copyright (c) 1997" -I"$Header: /tmp/reportlab/rl_addons/pyRXP/patches/differ.bat,v 1.2 2003/04/01 08:03:56 rgbecker Exp $Id:" -x"Entries" %1 %2 > %3
