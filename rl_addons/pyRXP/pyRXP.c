1c5294c6dead425951735426a5cf3b8645bb5322
rgbecker <unknown>
2003-04-01 16:10:20 +0000

    Sync with rxp 1.3.0

--- rl_addons/pyRXP/pyRXP.c
+++ rl_addons/pyRXP/pyRXP.c
@@ -2,9 +2,9 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/rl_addons/pyRXP/pyRXP.c?cvsroot=reportlab
-#$Header: /tmp/reportlab/rl_addons/pyRXP/pyRXP.c,v 1.18 2003/03/13 18:38:18 rgbecker Exp $
+#$Header: /tmp/reportlab/rl_addons/pyRXP/pyRXP.c,v 1.19 2003/04/01 16:10:20 rgbecker Exp $
  ****************************************************************************/
-static char* __version__=" $Id: pyRXP.c,v 1.18 2003/03/13 18:38:18 rgbecker Exp $ ";
+static char* __version__=" $Id: pyRXP.c,v 1.19 2003/04/01 16:10:20 rgbecker Exp $ ";
 #include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +24,7 @@ static char* __version__=" $Id: pyRXP.c,v 1.18 2003/03/13 18:38:18 rgbecker Exp 
 #include "stdio16.h"
 #include "version.h"
 #include "namespaces.h"
-#define VERSION "0.96"
+#define VERSION "0.97"
 #define MODULE "pyRXP"
 #define MAX_DEPTH 256
 static PyObject *moduleError;
