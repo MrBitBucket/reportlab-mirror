2159c216c0b49ea762604b86520421a1cafdfe7d
rgbecker <unknown>
2003-07-01 18:58:37 +0000

    Added linux2 flag

--- reportlab/lib/setup.py
+++ reportlab/lib/setup.py
@@ -2,19 +2,19 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/lib/setup.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/lib/setup.py,v 1.14 2002/07/24 19:56:37 andy_robinson Exp $
+#$Header: /tmp/reportlab/reportlab/lib/setup.py,v 1.15 2003/07/01 18:58:37 rgbecker Exp $
 if __name__=='__main__': #NO RUNTESTS
     import os, sys
     from distutils.core import setup, Extension
 
-    if sys.platform in ['win32', 'sunos5', 'freebsd4', 'aix4', 'mac', 'darwin']:
+    if sys.platform in ['linux2', 'win32', 'sunos5', 'freebsd4', 'aix4', 'mac', 'darwin']:
         LIBS=[]
     else:
         raise ValueError, "Don't know about platform:"+sys.platform
 
     setup(  name = "_rl_accel",
-            version = "0.2",
-            description = "Python Reportlab acceleretaor extensions",
+            version = "0.42",
+            description = "Python Reportlab accelerator extensions",
             author = "Robin Becker",
             author_email = "robin@reportlab.com",
             url = "http://www.reportlab.com",
