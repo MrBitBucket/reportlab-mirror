52e3dab329e400cdf6d150e53bb3d9517077ce2a
rgbecker <unknown>
2003-09-04 13:35:14 +0000

    Use strokeLineJoin=1 for 3d wedges

--- reportlab/graphics/charts/piecharts.py
+++ reportlab/graphics/charts/piecharts.py
@@ -1,7 +1,7 @@
 #copyright ReportLab Inc. 2000-2001
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/reportlab/graphics/charts/piecharts.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/graphics/charts/piecharts.py,v 1.32 2003/08/27 15:01:15 rgbecker Exp $
+#$Header: /tmp/reportlab/reportlab/graphics/charts/piecharts.py,v 1.33 2003/09/04 13:35:14 rgbecker Exp $
 # experimental pie chart script.  Two types of pie - one is a monolithic
 #widget with all top-level properties, the other delegates most stuff to
 #a wedges collection whic lets you customize the group or every individual
@@ -12,7 +12,7 @@
 This permits you to customize and pop out individual wedges;
 supports elliptical and circular pies.
 """
-__version__=''' $Id: piecharts.py,v 1.32 2003/08/27 15:01:15 rgbecker Exp $ '''
+__version__=''' $Id: piecharts.py,v 1.33 2003/09/04 13:35:14 rgbecker Exp $ '''
 
 import copy
 from math import sin, cos, pi
@@ -583,7 +583,7 @@ class Pie3d(Pie):
                 self.CX(i,1),self.CY(i,1),
                 self.OX(i,angle,1),self.OY(i,angle,1),
                 self.OX(i,angle,0),self.OY(i,angle,0)]
-            L.append((rd,Polygon(p, strokeColor=strokeColor, fillColor=fillColor,strokeWidth=strokeWidth)))
+            L.append((rd,Polygon(p, strokeColor=strokeColor, fillColor=fillColor,strokeWidth=strokeWidth,strokeLineJoin=1)))
 
     def draw(self):
         slices = self.slices
@@ -649,12 +649,13 @@ class Pie3d(Pie):
             cy1 = CY(i,1)
             #background shaded pie bottom
             g.add(Wedge(cx1,cy1,radiusx, lo, hi,yradius=radiusy,
-                            strokeColor=strokeColor,strokeWidth=strokeWidth,fillColor=fillColor))
+                            strokeColor=strokeColor,strokeWidth=strokeWidth,fillColor=fillColor,
+                            strokeLineJoin=1))
             #connect to top
             if lo < a0 < hi: angle0 = a0
             if lo < a1 < hi: angle1 = a1
             if 1:
-                p = ArcPath(strokeColor=strokeColor, fillColor=fillColor,strokeWidth=strokeWidth)
+                p = ArcPath(strokeColor=strokeColor, fillColor=fillColor,strokeWidth=strokeWidth,strokeLineJoin=1)
                 p.addArc(cx1,cy1,radiusx,angle0,angle1,yradius=radiusy,moveTo=1)
                 p.lineTo(OX(i,angle1,0),OY(i,angle1,0))
                 p.addArc(cx0,cy0,radiusx,angle0,angle1,yradius=radiusy,reverse=1)
@@ -671,7 +672,7 @@ class Pie3d(Pie):
             fillColor = style.fillColor
             strokeColor = style.strokeColor or fillColor
             T.append(Wedge(cx0,cy0,radiusx,lo,hi,yradius=radiusy,
-                            strokeColor=strokeColor,strokeWidth=strokeWidth,fillColor=fillColor))
+                            strokeColor=strokeColor,strokeWidth=strokeWidth,fillColor=fillColor,strokeLineJoin=1))
 
             text = labels[i]
             if text:
