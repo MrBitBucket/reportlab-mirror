b551ca24e53b1888ad7b6a9e2e0f74b44cafb827
dragan1 <unknown>
2003-09-11 15:24:07 +0000

    initial

---
+++ reportlab/test/test_rl_accel.py
@@ -0,0 +1,58 @@
+__version__=''' $Id'''
+__doc__='''basic tests.'''
+
+from reportlab.test import unittest
+from reportlab.test.utils import makeSuiteForClasses
+
+class RlAccelTestCase(unittest.TestCase):
+    
+    def testFpStr(self):
+        from _rl_accel import fp_str
+        assert fp_str(1,2,3)=='1 2 3'
+
+    def test_AsciiBase85Encode(self):
+        from _rl_accel import _AsciiBase85Encode
+        assert _AsciiBase85Encode('Dragan Andric')=='6ul^K@;[2RDIdd%@f~>'
+
+    def test_AsciiBase85Decode(self):
+        from _rl_accel import _AsciiBase85Decode
+        assert _AsciiBase85Decode('6ul^K@;[2RDIdd%@f~>')=='Dragan Andric'
+
+    def testEscapePDF(self):
+        from _rl_accel import escapePDF
+        assert escapePDF('(test)')=='\\(test\\)'
+        
+    def test_instanceEscapePDF(self):
+        from _rl_accel import _instanceEscapePDF
+        assert _instanceEscapePDF('', '(test)')=='\\(test\\)'
+
+    def testCalcChecksum(self):
+        from _rl_accel import calcChecksum
+        assert calcChecksum('test')==1952805748
+
+    def testStringWidth(self):
+        pass
+
+    def test_instanceStringWidth(self):
+        pass
+
+    def test_sameFrag(self):
+        pass
+
+    def testGetFontInfo(self):
+        pass
+
+    def testSetFontInfo(self):
+        pass
+
+def makeSuite():
+    # only run the tests if _rl_accel is present
+    try:
+        import _rl_accel
+        return makeSuiteForClasses(RlAccelTestCase)
+    except ImportError:
+        return None
+
+#noruntests
+if __name__ == "__main__":
+    unittest.TextTestRunner().run(makeSuite())
