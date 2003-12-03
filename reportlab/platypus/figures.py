9af4abf21934cb5179683cd512384fe7418909aa
johnprecedo <unknown>
2003-12-03 15:19:43 +0000

    Added a background attribute (default None) to most figures - if it's a colour, it creates a coloured rectangle under the figure before drawing it on the page. Useful for when putting figures onto a coloured background.

--- reportlab/platypus/figures.py
+++ reportlab/platypus/figures.py
@@ -1,9 +1,9 @@
 #copyright ReportLab Inc. 2000
 #see license.txt for license details
 #history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/docs/tools/platdemos.py?cvsroot=reportlab
-#$Header: /tmp/reportlab/reportlab/platypus/figures.py,v 1.7 2003/12/02 17:24:29 andy_robinson Exp $
+#$Header: /tmp/reportlab/reportlab/platypus/figures.py,v 1.8 2003/12/03 15:19:43 johnprecedo Exp $
 """This includes some demos of platypus for use in the API proposal"""
-__version__=''' $Id: figures.py,v 1.7 2003/12/02 17:24:29 andy_robinson Exp $ '''
+__version__=''' $Id: figures.py,v 1.8 2003/12/03 15:19:43 johnprecedo Exp $ '''
 
 import os
 
@@ -16,17 +16,20 @@ from reportlab.platypus import Flowable
 from reportlab.platypus import Paragraph
 from reportlab.lib.units import inch
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
+from reportlab.lib.validators import isColor
+from reportlab.lib.colors import toColor
 
 captionStyle = ParagraphStyle('Caption', fontName='Times-Italic', fontSize=10, alignment=TA_CENTER)
 
 class Figure(Flowable):
     def __init__(self, width, height, caption="",
-                 captionFont="Times-Italic", captionSize=12):
+                 captionFont="Times-Italic", captionSize=12, background=None):
         Flowable.__init__(self)
         self.width = width
         self.figureHeight = height
         self.captionHeight = 0  # work out later
         self.caption = caption
+        self.background = background
         self.captionStyle = ParagraphStyle(
             'Caption',
             fontName=captionFont,
@@ -53,11 +56,31 @@ class Figure(Flowable):
         self.canv.translate(self.dx, 0)
         self.drawCaption()
         self.canv.translate(0, self.captionHeight)
+        if self.background:
+            self.drawBackground()
         self.drawBorder()
         self.drawFigure()
 
     def drawBorder(self):
         self.canv.rect(0, 0, self.width, self.figureHeight)
+
+    def _doBackground(self, color):
+        self.canv.saveState()
+        self.canv.setFillColor(self.background)
+        self.canv.rect(0, 0, self.width, self.figureHeight, fill=1)
+        self.canv.restoreState()
+
+    def drawBackground(self):
+        """For use when using a figure on a differently coloured background.
+        Allows you to specify a colour to be used as a background for the figure."""
+        if isColor(self.background):
+            self._doBackground(self.background)
+        else:
+            try:
+                c = toColor(self.background)
+                self._doBackground(c)
+            except:
+                pass
 
     def drawCaption(self):
         self.captionPara.drawOn(self.canv, 0, 0)
@@ -88,10 +111,11 @@ def drawPage(canvas,x, y, width, height):
 class PageFigure(Figure):
     """Shows a blank page in a frame, and draws on that.  Used in
     illustrations of how PLATYPUS works."""
-    def __init__(self):
+    def __init__(self, background=None):
         Figure.__init__(self, 3*inch, 3*inch)
         self.caption = 'Figure 1 - a blank page'
         self.captionStyle = captionStyle
+        self.background = background
 
     def drawVirtualPage(self):
         pass
@@ -112,9 +136,10 @@ class PlatPropFigure1(PageFigure):
 
 class FlexFigure(Figure):
     """Base for a figure class with a caption. Can grow or shrink in proportion"""
-    def __init__(self, width, height, caption):
+    def __init__(self, width, height, caption, background=None):
         Figure.__init__(self, width, height, caption,
-                        captionFont="Helvetica-Oblique", captionSize=8)
+                        captionFont="Helvetica-Oblique", captionSize=8,
+                        background=None)
         self.shrinkToFit = 1 #if set and wrap is too tight, shrinks
         self.growToFit = 1 #if set and wrap is too tight, shrinks
         self.scaleFactor = 1
@@ -126,6 +151,7 @@ class FlexFigure(Figure):
             alignment=TA_CENTER
             )
         self._scaledWidth = None
+        self.background = background
 
     def wrap(self, availWidth, availHeight):
         "Rescale to fit according to the rules, but only once"
@@ -144,12 +170,12 @@ class FlexFigure(Figure):
 
 class ImageFigure(FlexFigure):
     """Image with a caption below it"""
-    def __init__(self, filename, caption):
+    def __init__(self, filename, caption, background=None):
         assert os.path.isfile(filename), 'image file %s not found' % filename
         from reportlab.lib.utils import ImageReader
         w, h = ImageReader(filename).getSize()
         self.filename = filename
-        FlexFigure.__init__(self, w, h, caption)
+        FlexFigure.__init__(self, w, h, caption, background)
 
     def drawFigure(self):
         self.canv.drawInlineImage(self.filename,
@@ -158,14 +184,15 @@ class ImageFigure(FlexFigure):
 
 class DrawingFigure(FlexFigure):
     """Drawing with a caption below it.  Clunky, scaling fails."""
-    def __init__(self, modulename, classname, caption, baseDir=None):
+    def __init__(self, modulename, classname, caption, baseDir=None, background=None):
         module = recursiveImport(modulename, baseDir)
         klass = getattr(module, classname)
         self.drawing = klass()
         FlexFigure.__init__(self,
                             self.drawing.width,
                             self.drawing.height,
-                            caption)
+                            caption,
+                            background)
         self.growToFit = 1
         
     def drawFigure(self):
@@ -221,14 +248,13 @@ if _hasPageCatcher:
         """PageCatcher page with a caption below it.  Presumes A4, Portrait.
         This needs our commercial PageCatcher product, or you'll get a blank."""
 
-            
-        def __init__(self, filename, pageNo, caption, width=595, height=842):
+        def __init__(self, filename, pageNo, caption, width=595, height=842, background=None):
             self.dirname, self.filename = os.path.split(filename)
             if self.dirname == '':
                 self.dirname = os.curdir
             self.pageNo = pageNo
             self.formName = self.getFormName(self.filename, self.pageNo) + '_' + str(pageNo)
-            FlexFigure.__init__(self, width, height, caption)
+            FlexFigure.__init__(self, width, height, caption, background)
             #print 'self.width=%0.2f, self.figureHeight=%0.2f' % (self.width, self.figureHeight)
             
 
@@ -254,14 +280,14 @@ if _hasPageCatcher:
         """PageCatcher page with a caption below it.  Size to be supplied."""
         # This should merge with PageFigure into one class that reuses
         # form information to determine the page orientation...
-        def __init__(self, filename, pageNo, caption, width, height):
+        def __init__(self, filename, pageNo, caption, width, height, background=None):
             self.dirname, self.filename = os.path.split(filename)
             if self.dirname == '':
                 self.dirname = os.curdir
             self.pageNo = pageNo
             self.formName = self.getFormName(self.filename, self.pageNo) + '_' + str(pageNo)
-            FlexFigure.__init__(self, width, height, caption)
-            
+            FlexFigure.__init__(self, width, height, caption, background)
+
         def drawFigure(self):
             self.canv.saveState()
             if not self.canv.hasForm(self.formName):
