1cdef596f945ee6fecd44308c97027c27280231e
rgbecker <unknown>
2004-03-23 12:55:44 +0000

    Allow to run in a compact distro

--- reportlab/test/test_lib_utils.py
+++ reportlab/test/test_lib_utils.py
@@ -5,14 +5,30 @@ import reportlab
 from reportlab.test import unittest
 from reportlab.test.utils import makeSuiteForClasses
 from reportlab.lib import colors
-from reportlab.lib.utils import recursiveImport, \
-                                 recursiveGetAttr, \
-                                recursiveSetAttr
-
-
+from reportlab.lib.utils import recursiveImport, recursiveGetAttr, recursiveSetAttr, rl_isfile, \
+                                isCompactDistro
 
 class ImporterTestCase(unittest.TestCase):
     "Test import utilities"
+
+    def setUp(self):
+        try:
+            from tempfile import mkdtemp
+            self._tempdir = mkdtemp()
+        except:
+            from tempfile import gettempdir
+            import time
+            self._tempdir = os.path.join(gettempdir(),'tmp_%d' % time.time())
+            os.mkdir(self._tempdir,0700)
+        _testmodulename = 'test_module.py'
+        f = open(_testmodulename,'w')
+        f.write('__all__=[]\n')
+        f.close()
+        self._testmodulename = _testmodulename[:-3]
+
+    def tearDown(self):
+        from shutil import rmtree
+        rmtree(self._tempdir,1)
 
     def test1(self):
         "try stuff known to be in the path"
@@ -21,35 +37,37 @@ class ImporterTestCase(unittest.TestCase):
         assert m1 == reportlab.pdfgen.canvas
 
     def test2(self):
-        "try under a directory NOT on the path"
+        "try under a well known directory NOT on the path"
         D = os.path.join(os.path.dirname(reportlab.__file__), 'tools','pythonpoint')
         fn = os.path.join(D,'stdparser.py')
-        if os.path.isfile(fn) or os.path.isfile(fn+'c') or os.path.isfile(fn+'o'):
+        if rl_isfile(fn) or rl_isfile(fn+'c') or rl_isfile(fn+'o'):
             m1 = recursiveImport('stdparser', baseDir=D)
 
     def test3(self):
         "ensure CWD is on the path"
-        rlDir = os.path.dirname(reportlab.__file__)
-        testDir = os.path.join(rlDir, 'test')
-        cwd = os.getcwd()
-        os.chdir(testDir)
-        m1 = recursiveImport('test_lib_colors')
+        try:
+            cwd = os.getcwd()
+            os.chdir(self._tempdir)
+            m1 = recursiveImport(self._testmodulename)
+        finally:
+            os.chdir(cwd)
 
     def test4(self):
         "ensure noCWD removes current dir from path"
-        rlDir = os.path.dirname(reportlab.__file__)
-        testDir = os.path.join(rlDir, 'test')
-        cwd = os.getcwd()
-        os.chdir(testDir)
-        import sys
         try:
-            del sys.modules['test_lib_colors']
-        except KeyError:
-            pass
-        self.assertRaises(ImportError,
-                          recursiveImport,
-                          'test_lib_colors',
-                          noCWD=1)
+            cwd = os.getcwd()
+            os.chdir(self._tempdir)
+            import sys
+            try:
+                del sys.modules[self._testmodulename]
+            except KeyError:
+                pass
+            self.assertRaises(ImportError,
+                              recursiveImport,
+                              'test_lib_colors',
+                              noCWD=1)
+        finally:
+            os.chdir(cwd)
 
     def test5(self):
         "recursive attribute setting/getting on modules"
@@ -69,17 +87,9 @@ class ImporterTestCase(unittest.TestCase):
         theMax = recursiveGetAttr(drawing, 'barchart.valueAxis.valueMax')
         assert theMax == 72
 
-
-
-
-
-
-
-
 def makeSuite():
     return makeSuiteForClasses(ImporterTestCase)
 
 
-#noruntests
-if __name__ == "__main__":
+if __name__ == "__main__": #noruntests
     unittest.TextTestRunner().run(makeSuite())
