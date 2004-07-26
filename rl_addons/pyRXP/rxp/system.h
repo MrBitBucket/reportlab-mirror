9fec30620b216d32a47c7766197b3c1bf4d54448
rgbecker <unknown>
2004-07-26 08:56:04 +0000

    Attempt to fix LONG_LONG defn problem

--- rl_addons/pyRXP/rxp/system.h
+++ rl_addons/pyRXP/rxp/system.h
@@ -1,7 +1,5 @@
-#ifndef PY_LONG_LONG
-#include "pyconfig.h"
-#endif
-
+#ifndef _RXP_SYSTEM_H
+#define _RXP_SYSTEM_H
 #define SOCKETS_IMPLEMENTED
 
 #define STD_API
@@ -12,9 +10,18 @@
 #	ifndef WIN32
 #		define WIN32
 #	endif
+#	ifndef PY_LONG_LONG
+#		define PY_LONG_LONG __int64
+#	endif
+#endif
+#ifndef PY_LONG_LONG
+#	if defined(__GNUC__)
+#		define PY_LONG_LONG long long
+#	endif
 #endif
 
 void *Malloc(int bytes);
 void *Realloc(void *mem, int bytes);
 void Free(void *mem);
 void CFree(void *mem);
+#endif
