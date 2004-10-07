0b837c5179412eb87e04177cc5b05e7d38400a25
rgbecker <unknown>
2004-10-07 17:51:31 +0000

    pdfutils.py: added _fusc

--- reportlab/pdfbase/pdfutils.py
+++ reportlab/pdfbase/pdfutils.py
@@ -427,3 +427,27 @@ def readJPEGInfo(image):
                 #read length and skip the data
                 x = struct.unpack('BB', image.read(2))
                 image.seek( (x[0] << 8) + x[1] - 2, 1)
+
+class _fusc:
+    def __init__(self,k, n):
+        assert k, 'Argument k should be a non empty string'
+        self._k = k
+        self._klen = len(k)
+        self._n = int(n) or 7
+
+    def encrypt(self,s):
+        return self.__rotate(_AsciiBase85Encode(''.join(map(chr,self.__fusc(map(ord,s))))),self._n)
+
+    def decrypt(self,s):
+        return ''.join(map(chr,self.__fusc(map(ord,_AsciiBase85Decode(self.__rotate(s,-self._n))))))
+
+    def __rotate(self,s,n):
+        l = len(s)
+        if n<0: n = l+n
+        n %= l
+        if not n: return s
+        return s[-n:]+s[:l-n]
+
+    def __fusc(self,s):
+        slen = len(s)
+        return map(lambda x,y: x ^ y,s,map(ord,((int(slen/self._klen)+1)*self._k)[:slen]))
