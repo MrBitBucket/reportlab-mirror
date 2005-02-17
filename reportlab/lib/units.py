b08748680b3c151768e70c5c9b45e6b00419f6a0
rgbecker <unknown>
2005-02-17 08:43:01 +0000

    units.py: fix pica conversion thanks to Paul McNett

--- reportlab/lib/units.py
+++ reportlab/lib/units.py
@@ -17,7 +17,7 @@ def toLength(s):
         if s[-2:]=='pt': return float(s[:-2])
         if s[-1:]=='i': return float(s[:-1])*inch
         if s[-2:]=='mm': return float(s[:-2])*mm
-        if s[-4:]=='pica': return float(s[:-2])*pica
+        if s[-4:]=='pica': return float(s[:-4])*pica
         return float(s)
     except:
         raise ValueError, "Can't convert '%s' to length" % s
