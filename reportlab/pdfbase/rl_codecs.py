30af350646158e5624f4df884920b1e30192ac8c
rgbecker <unknown>
2006-05-17 10:58:04 +0000

    rl_codecs: slight speedup

--- reportlab/pdfbase/rl_codecs.py
+++ reportlab/pdfbase/rl_codecs.py
@@ -986,11 +986,11 @@ class RL_Codecs:
         if rexceptions: encoding_map.update(rexceptions)
         ### Codec APIs
         class Codec(codecs.Codec):
-            def encode(self,input,errors='strict',codecs=codecs,encoding_map=encoding_map):
-                return codecs.charmap_encode(input,errors,encoding_map)
+            def encode(self,input,errors='strict',charmap_encode=codecs.charmap_encode,encoding_map=encoding_map):
+                return charmap_encode(input,errors,encoding_map)
 
-            def decode(self,input,errors='strict',codecs=codecs,decoding_map=decoding_map):
-                return codecs.charmap_decode(input,errors,decoding_map)
+            def decode(self,input,errors='strict',charmap_decode=codecs.charmap_decode,decoding_map=decoding_map):
+                return charmap_decode(input,errors,decoding_map)
 
         class StreamWriter(Codec,codecs.StreamWriter):
             pass
