23a1fd3aca016c467f83c6ecc7c83b11757d6658
andy <unknown>
2006-05-18 15:11:23 +0000

    changed version and added warning for old Python users

--- reportlab/__init__.py
+++ reportlab/__init__.py
@@ -3,7 +3,17 @@
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
 __version__=''' $Id$ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "1.20"
+Version = "2.0"
+
+import sys
+
+if sys.version_info[0:2] < (2, 3):
+    warning = """The trunk of reportlab requires Python 2.3 or higher.
+    Any older applications should either use released versions beginning
+    with 1.x (e.g. 1.21), or snapshots or checkouts from our 'version1'
+    branch.
+    """
+    raise ImportError("reportlab needs Python 2.3 or higher", warning)
 
 def getStory(context):
     if context.target == 'UserGuide':
