649239346a7e18a9f54dd16b1f89de875c1a1f0e
rgbecker <unknown>
2008-09-03 15:55:40 +0000

    move utils one level up for reportlab-2.2

--- utils/license.c
+++
@@ -1,7 +0,0 @@
-/****************************************************************************
-#copyright ReportLab Inc. 2000
-#see license.txt for license details
-#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/utils/license.c?cvsroot=reportlab
-#$Header: /cvsroot/reportlab/utils/copyr.txt,v 1.1 2000/10/24 13:56:02 rgbecker Exp $
- ****************************************************************************/
-static __version__=" $Id$ "
--- utils/license.py
+++
@@ -1,6 +0,0 @@
-#!/bin/env python
-#copyright ReportLab Inc. 2000
-#see license.txt for license details
-#history http://cvs.sourceforge.net/cgi-bin/cvsweb.cgi/utils/license.py?cvsroot=reportlab
-#$Header: /cvsroot/reportlab/utils/copyr.txt,v 1.1 2000/10/24 13:56:02 rgbecker Exp $
-__version__=''' $Id$ '''
