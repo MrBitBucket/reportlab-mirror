4a7a71b4976fa00cd1d8b5da4e3ad92dd61b8f5b
andy <unknown>
2009-01-23 15:11:14 +0000

    nearly version 3

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -3,7 +3,7 @@
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
 __version__=''' $Id$ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "2.2"
+Version = "2.3pre"
 
 import sys
 
