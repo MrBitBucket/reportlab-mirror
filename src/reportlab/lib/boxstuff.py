dc47ed83b8c751609f4b2dfa658ca5abc50ac05f
rptlab <unknown>
2009-01-28 12:25:33 +0000

    reportlab: fix rest comments in boxstuff.py

--- src/reportlab/lib/boxstuff.py
+++ src/reportlab/lib/boxstuff.py
@@ -9,22 +9,21 @@ def aspectRatioFix(preserve,anchor,x,y,width,height,imWidth,imHeight):
     It first normalizes for two cases:
     - if the width is None, it assumes imWidth
     - ditto for height
-    - if width or height is negative, it adjusts x or y and makes
-      them positive
+    - if width or height is negative, it adjusts x or y and makes them positive
 
-    
-    
-    Given 
-    (a) the enclosing box (defined by x,y,width,height where x,y is the
-        lower left corner) which you wish to position the image in, and 
+    Given
+    (a) the enclosing box (defined by x,y,width,height where x,y is the \
+        lower left corner) which you wish to position the image in, and
     (b) the image size (imWidth, imHeight), and
-    (c) the 'anchor point' as a point of the compass - n,s,e,w,ne,se etc
+    (c) the 'anchor point' as a point of the compass - n,s,e,w,ne,se etc \
         and c for centre,
+
     this should return the position at which the image should be drawn,
     as well as a scale factor indicating what scaling has happened.
-    
+
     It returns the parameters which would be used to draw the image
-    without any adjustments: 
+    without any adjustments:
+
         x,y, width, height, scale
 
     used in canvas.drawImage and drawInlineImage
