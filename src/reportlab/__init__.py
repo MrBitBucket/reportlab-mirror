309e019d67ae804a669019919d73dbb0fc1bcdf4
rgbecker <unknown>
2010-09-29 10:44:00 +0000

    trunk: prepare for version 2.5 tag

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -3,7 +3,7 @@
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
 __version__=''' $Id$ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "2.4"
+Version = "2.5"
 
 import sys
 
