254a23d51dcd0c90285a0064bf39e7dd03997d1f
Andy Robinson <andy@reportlab.com>
2013-04-03 12:00:32 +0100

    bumped the version

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -3,7 +3,7 @@
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
 __version__=''' $Id$ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "2.6"
+Version = "2.7"
 
 import sys
 
