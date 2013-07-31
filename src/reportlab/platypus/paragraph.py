546356ffc04d336a7d811070cb5af076e134b30a
robin <unknown>
2013-07-31 16:27:08 +0100

    paragraph.py: python-3.3 allows u prefix

--- src/reportlab/platypus/paragraph.py
+++ src/reportlab/platypus/paragraph.py
@@ -25,36 +25,36 @@ import re
 #thanks to Dirk Holtwick for helpful discussions/insight
 #on this one
 _wsc = ''.join((
-    '\u0009',  # HORIZONTAL TABULATION
-    '\u000A',  # LINE FEED
-    '\u000B',  # VERTICAL TABULATION
-    '\u000C',  # FORM FEED
-    '\u000D',  # CARRIAGE RETURN
-    '\u001C',  # FILE SEPARATOR
-    '\u001D',  # GROUP SEPARATOR
-    '\u001E',  # RECORD SEPARATOR
-    '\u001F',  # UNIT SEPARATOR
-    '\u0020',  # SPACE
-    '\u0085',  # NEXT LINE
+    u'\u0009',  # HORIZONTAL TABULATION
+    u'\u000A',  # LINE FEED
+    u'\u000B',  # VERTICAL TABULATION
+    u'\u000C',  # FORM FEED
+    u'\u000D',  # CARRIAGE RETURN
+    u'\u001C',  # FILE SEPARATOR
+    u'\u001D',  # GROUP SEPARATOR
+    u'\u001E',  # RECORD SEPARATOR
+    u'\u001F',  # UNIT SEPARATOR
+    u'\u0020',  # SPACE
+    u'\u0085',  # NEXT LINE
     #u'\u00A0', # NO-BREAK SPACE
-    '\u1680',  # OGHAM SPACE MARK
-    '\u2000',  # EN QUAD
-    '\u2001',  # EM QUAD
-    '\u2002',  # EN SPACE
-    '\u2003',  # EM SPACE
-    '\u2004',  # THREE-PER-EM SPACE
-    '\u2005',  # FOUR-PER-EM SPACE
-    '\u2006',  # SIX-PER-EM SPACE
-    '\u2007',  # FIGURE SPACE
-    '\u2008',  # PUNCTUATION SPACE
-    '\u2009',  # THIN SPACE
-    '\u200A',  # HAIR SPACE
-    '\u200B',  # ZERO WIDTH SPACE
-    '\u2028',  # LINE SEPARATOR
-    '\u2029',  # PARAGRAPH SEPARATOR
-    '\u202F',  # NARROW NO-BREAK SPACE
-    '\u205F',  # MEDIUM MATHEMATICAL SPACE
-    '\u3000',  # IDEOGRAPHIC SPACE
+    u'\u1680',  # OGHAM SPACE MARK
+    u'\u2000',  # EN QUAD
+    u'\u2001',  # EM QUAD
+    u'\u2002',  # EN SPACE
+    u'\u2003',  # EM SPACE
+    u'\u2004',  # THREE-PER-EM SPACE
+    u'\u2005',  # FOUR-PER-EM SPACE
+    u'\u2006',  # SIX-PER-EM SPACE
+    u'\u2007',  # FIGURE SPACE
+    u'\u2008',  # PUNCTUATION SPACE
+    u'\u2009',  # THIN SPACE
+    u'\u200A',  # HAIR SPACE
+    u'\u200B',  # ZERO WIDTH SPACE
+    u'\u2028',  # LINE SEPARATOR
+    u'\u2029',  # PARAGRAPH SEPARATOR
+    u'\u202F',  # NARROW NO-BREAK SPACE
+    u'\u205F',  # MEDIUM MATHEMATICAL SPACE
+    u'\u3000',  # IDEOGRAPHIC SPACE
     ))
 _wsc_re_split=re.compile('[%s]+'% re.escape(_wsc)).split
 
