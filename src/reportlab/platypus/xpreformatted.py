df52fe8e0ac604f84ff1d21d29c005a72427193e
robin <unknown>
2013-11-19 15:29:34 +0000

    xpreformatted.py:  add missing import

--- src/reportlab/platypus/xpreformatted.py
+++ src/reportlab/platypus/xpreformatted.py
@@ -6,6 +6,7 @@ __doc__='''A 'rich preformatted text' widget allowing internal markup'''
 from reportlab.lib import PyFontify
 from reportlab.platypus.paragraph import Paragraph, cleanBlockQuotedText, _handleBulletWidth, \
      ParaLines, _getFragWords, stringWidth, getAscentDescent, imgVRange, imgNormV
+from reportlab.lib.utils import isSeq
 from reportlab.platypus.flowables import _dedenter
 
 def _getFragLines(frags):
