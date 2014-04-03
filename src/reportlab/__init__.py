fc9a50e8dc8ff2ecbca99400c0d8326e8e88ee1f
robin <unknown>
2014-04-03 11:42:53 +0100

    bump version to 3.1.5

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -3,7 +3,7 @@
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
 __version__=''' $Id$ '''
 __doc__="""The Reportlab PDF generation library."""
-Version = "3.1.4"
+Version = "3.1.5"
 
 import sys, os, imp
 
