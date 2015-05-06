42a322bde21f1775543e905fedc253b94b066f8f
robin <unknown>
2015-05-06 15:05:17 +0100

    clean up pdfform.py (prompted by jvalenzuela @ bitbucket); version-->3.1.56

--- src/reportlab/pdfbase/pdfform.py
+++ src/reportlab/pdfbase/pdfform.py
@@ -58,18 +58,18 @@ def textFieldRelative(canvas, title, xR, yR, width, height, value="", maxlen=100
     (xA, yA) = canvas.absolutePosition(xR,yR)
     return textFieldAbsolute(canvas, title, xA, yA, width, height, value, maxlen, multiline)
 
-def buttonFieldAbsolute(canvas, title, value, x, y):
+def buttonFieldAbsolute(canvas, title, value, x, y, width=16.7704, height=14.907):
     """Place a check button field on the current page
         with name title and default value value (one of "Yes" or "Off")
         at ABSOLUTE position (x,y).
     """
     theform = getForm(canvas)
-    return theform.buttonField(canvas, title, value, x, y)
+    return theform.buttonField(canvas, title, value, x, y, width=width, height=height)
 
-def buttonFieldRelative(canvas, title, value, xR, yR):
+def buttonFieldRelative(canvas, title, value, xR, yR, width=16.7704, height=14.907):
     "same as buttonFieldAbsolute except the x and y are relative to the canvas coordinate transform"
     (xA, yA) = canvas.absolutePosition(xR,yR)
-    return buttonFieldAbsolute(canvas, title, value, xA, yA)
+    return buttonFieldAbsolute(canvas, title, value, xA, yA, width=width, height=height)
 
 def selectFieldAbsolute(canvas, title, value, options, x, y, width, height):
     """Place a select field (drop down list) on the current page
@@ -84,39 +84,6 @@ def selectFieldRelative(canvas, title, value, options, xR, yR, width, height):
     "same as textFieldAbsolute except the x and y are relative to the canvas coordinate transform"
     (xA, yA) = canvas.absolutePosition(xR,yR)
     return selectFieldAbsolute(canvas, title, value, options, xA, yA, width, height)
-
-def test1():
-    from reportlab.pdfgen import canvas
-    fn = "formtest1.pdf"
-    c = canvas.Canvas(fn)
-    # first page
-    c.setFont("Courier", 10)
-    c.drawString(100, 500, "hello world")
-    textFieldAbsolute(c, "fieldA", 100, 600, 100, 20, "default value")
-    textFieldAbsolute(c, "fieldB", 100, 300, 100, 50, "another default value", multiline=1)
-    selectFieldAbsolute(c, "fieldC", "France", ["Canada", "France", "China"], 100, 200, 100, 20)
-    c.rect(100, 600, 100, 20)
-    buttonFieldAbsolute(c, "field2", "Yes", 100, 700)
-    c.rect(100, 700, 20, 20)
-    buttonFieldAbsolute(c, "field3", "Off", 100, 800)
-    c.rect(100, 800, 20, 20)
-    # second page
-    c.showPage()
-    c.setFont("Helvetica", 7)
-    c.translate(50, 20)
-    c.drawString(100, 500, "hello world")
-    textFieldRelative(c, "fieldA_1", 100, 600, 100, 20, "default value 2")
-    c.setStrokeColorRGB(1,0,0)
-    c.setFillColorRGB(0,1,0.5)
-    textFieldRelative(c, "fieldB_1", 100, 300, 100, 50, "another default value 2", multiline=1)
-    selectFieldRelative(c, "fieldC_1", "France 1", ["Canada 0", "France 1", "China 2"], 100, 200, 100, 20)
-    c.rect(100, 600, 100, 20)
-    buttonFieldRelative(c, "field2_1", "Yes", 100, 700)
-    c.rect(100, 700, 20, 20)
-    buttonFieldRelative(c, "field3_1", "Off", 100, 800)
-    c.rect(100, 800, 20, 20)
-    c.save()
-    print("wrote", fn)
 
 #==========================end of public interfaces
 
@@ -163,16 +130,17 @@ class AcroForm(PDFObject):
               font=font, fontsize=fontsize, R=R, G=G, B=B)
         self.fields.append(field)
         canvas._addAnnotation(field)
-    def buttonField(self, canvas, title, value, xmin, ymin):
+    def buttonField(self, canvas, title, value, xmin, ymin, width=16.7704, height=14.907):
         # determine the page ref
         doc = canvas._doc
         page = doc.thisPageRef()
-        field = ButtonField(title, value, xmin, ymin, page)
+        field = ButtonField(title, value, xmin, ymin, page, width=width, height=height)
         self.fields.append(field)
         canvas._addAnnotation(field)
     def format(self, document):
         from reportlab.pdfbase.pdfdoc import PDFArray
-        proxy = PDFPattern(FormPattern, Resources=getattr(self,'resources',GLOBALRESOURCES),
+        proxy = PDFPattern(FormPattern,
+                    Resources=getattr(self,'resources',None) or FormResources(),
                     NeedAppearances=getattr(self,'needAppearances','false'),
                     fields=PDFArray(self.fields), SigFlags=getattr(self,'sigFlags',0))
         return proxy.format(document)
@@ -193,12 +161,13 @@ def FormFontsDictionary():
     fontsdictionary.__RefOnly__ = 1
     for fullname, shortname in FORMFONTNAMES.items():
         fontsdictionary[shortname] = FormFont(fullname, shortname)
-    fontsdictionary["ZaDb"] = ZADB.clone()
+    fontsdictionary["ZaDb"] = PDFPattern(ZaDbPattern)
     return fontsdictionary
 
 def FormResources():
     return PDFPattern(FormResourcesDictionaryPattern,
-                      Encoding=ENCODING.clone(), Font=GLOBALFONTSDICTIONARY)
+                      Encoding=PDFPattern(EncodingPattern,PDFDocEncoding=PDFPattern(PDFDocEncodingPattern)),
+                      Font=FormFontsDictionary())
 
 ZaDbPattern = [
 ' <<'
@@ -395,7 +364,7 @@ PDFDocEncodingPattern = [
 
 def FormFont(BaseFont, Name):
     from reportlab.pdfbase.pdfdoc import PDFName
-    return PDFPattern(FormFontPattern, BaseFont=PDFName(BaseFont), Name=PDFName(Name), Encoding=PDFDOCENC.clone())
+    return PDFPattern(FormFontPattern, BaseFont=PDFName(BaseFont), Name=PDFName(Name), Encoding=PDFPattern(PDFDocEncodingPattern))
 
 FormFontPattern = [
 '<<',
@@ -412,12 +381,7 @@ FormFontPattern = [
 '>>' ]
 
 def resetPdfForm():
-    global PDFDOCENC,ENCODING,GLOBALFONTSDICTIONARY,GLOBALRESOURCES,ZADB
-    PDFDOCENC = PDFPattern(PDFDocEncodingPattern)
-    ENCODING = PDFPattern(EncodingPattern, PDFDocEncoding=PDFDOCENC.clone())
-    ZADB = PDFPattern(ZaDbPattern)
-    GLOBALFONTSDICTIONARY = FormFontsDictionary()
-    GLOBALRESOURCES = FormResources()
+    pass
 from reportlab.rl_config import register_reset
 register_reset(resetPdfForm)
 resetPdfForm()
@@ -520,17 +484,21 @@ SelectFieldPattern = [
 ["Selected"],'\r\n',
 '>>']
 
-def ButtonField(title, value, xmin, ymin, page):
+def ButtonField(title, value, xmin, ymin, page, width=16.7704, height=14.907):
     if value not in ("Yes", "Off"):
         raise ValueError("button value must be 'Yes' or 'Off': "+repr(value))
-    (dx, dy) = (16.77036, 14.90698)
+    fontSize = (11.3086/14.907)*height
+    dx = (3.6017/16.7704)*width
+    dy = (3.3881/14.907)*height
     return PDFPattern(ButtonFieldPattern,
                       Name=PDFString(title),
-                      xmin=xmin, ymin=ymin, xmax=xmin+dx, ymax=ymin+dy,
-                      Hide=HIDE,
-                      APDOff=APDOFF,
-                      APDYes=APDYES,
-                      APNYes=APNYES,
+                      xmin=xmin, ymin=ymin, xmax=xmin+width, ymax=ymin+width,
+                      Hide=PDFPattern(['<< /S  /Hide >>']),
+                      APDOff=ButtonStream('0.749 g 0 0 %(width)s %(height)s re f\r\n' % vars(), width=width, height=height),
+                      APDYes=ButtonStream('0.749 g 0 0 %(width)s %(height)s re f q 1 1 %(width)s %(height)s re W n BT /ZaDb %(fontSize)s Tf 0 g 1 0 0 1 %(dx)s %(dy)s Tm (4) Tj ET\r\n' % vars(),
+                                            width=width, height=height),
+                      APNYes=ButtonStream('q 1 1 %(width)s %(height)s re W n BT /ZaDb %(fontSize)s Tf 0 g   1 0 0 1 %(dx)s %(dy)s Tm (4) Tj ET Q\r\n' % vars(),
+                                            width=width, height=height),
                       Value=PDFName(value),
                       Page=page)
 
@@ -595,40 +563,21 @@ PDFString('\376\377'),
 ["Value"], '\r\n',
 ' >>']
 
-HIDE = PDFPattern([
-'<< '
-'/S '
-' /Hide '
-'>>'])
 
-def buttonStreamDictionary():
+def buttonStreamDictionary(width=16.7704, height=14.907):
     "everything except the length for the button appearance streams"
     result = PDFDictionary()
     result["SubType"] = "/Form"
-    result["BBox"] = "[0 0 16.77036 14.90698]"
+    result["BBox"] = "[0 0 %(width)s %(height)s]" % vars()
     font = PDFDictionary()
-    font["ZaDb"] = ZADB.clone()
+    font["ZaDb"] = PDFPattern(ZaDbPattern)
     resources = PDFDictionary()
     resources["ProcSet"] = "[ /PDF /Text ]"
     resources["Font"] = font
     result["Resources"] = resources
     return result
 
-def ButtonStream(content):
-    dict = buttonStreamDictionary()
-    result = PDFStream(dict, content)
+def ButtonStream(content, width=16.7704, height=14.907):
+    result = PDFStream(buttonStreamDictionary(width=width,height=height), content)
     result.filters = []
     return result
-
-APDOFF = ButtonStream('0.749 g 0 0 16.7704 14.907 re f\r\n')
-APDYES = ButtonStream(
-'0.749 g 0 0 16.7704 14.907 re f q 1 1 14.7704 12.907 re W '+
-'n BT /ZaDb 11.3086 Tf 0 g  1 0 0 1 3.6017 3.3881 Tm (4) Tj ET\r\n')
-APNYES = ButtonStream(
-'q 1 1 14.7704 12.907 re W n BT /ZaDb 11.3086 Tf 0 g  1 0 0 1 3.6017 3.3881 Tm (4) Tj ET Q\r\n')
-
-
-#==== script interpretation
-
-if __name__=="__main__":
-    test1()
