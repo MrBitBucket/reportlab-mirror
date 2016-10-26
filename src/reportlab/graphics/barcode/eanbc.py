2a76eff468f0f4b35c8c6cc7f46d4931e877ed48
robin <unknown>
2016-10-26 13:20:33 +0100

    improve UPCA barcode contribution by Kyle McFarlane https://bitbucket.org/kylemacfarlane/

--- src/reportlab/graphics/barcode/eanbc.py
+++ src/reportlab/graphics/barcode/eanbc.py
@@ -350,6 +350,45 @@ class UPCA(Ean13BarcodeWidget):
     _1csw = 1
     _nbars = 1+7*11+2*3+5
 
+    #these methods contributed by Kyle Macfarlane
+    #https://bitbucket.org/kylemacfarlane/
+    def _encode_left(self,s,a):
+        cp = self._lhconvert[s[0]]      #convert the left hand numbers
+        _left = self._left[0]
+        z = ord('0')
+        for i,c in enumerate(s[0:self._start_right]):
+            a(_left[ord(c)-z])
+
+    def _short_bar(self,i):
+        i += 9 - self._lquiet
+        return self.humanReadable and ((18<i<55) or (57<i<93))
+
+    def _add_human_readable(self,s,gAdd):
+        barWidth = self.barWidth
+        fontSize = self.fontSize
+        textColor = self.textColor
+        fontName = self.fontName
+        fth = fontSize*1.2
+        # draw the num below the line.
+        c = s[0]
+        w = stringWidth(c,fontName,fontSize)
+        x = self.x+barWidth*(self._lquiet-8)
+        y = self.y + 0.2*fth
+
+        gAdd(String(x,y,c,fontName=fontName,fontSize=fontSize,fillColor=textColor))
+        x = self.x + (38-9+self._lquiet)*barWidth
+
+        c = s[1:6]
+        gAdd(String(x,y,c,fontName=fontName,fontSize=fontSize,fillColor=textColor,textAnchor='middle'))
+
+        x += 36*barWidth
+        c = s[6:11]
+        gAdd(String(x,y,c,fontName=fontName,fontSize=fontSize,fillColor=textColor,textAnchor='middle'))
+
+        x += 32*barWidth
+        c = s[11]
+        gAdd(String(x,y,c,fontName=fontName,fontSize=fontSize,fillColor=textColor))
+
 class Ean5BarcodeWidget(Ean13BarcodeWidget):
     """
     EAN-5 barcodes can print the human readable price, set:
