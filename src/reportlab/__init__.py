7ed517e02a9bda0b8b12affc16834017b2a30f67
robin <unknown>
2016-11-16 16:05:49 +0000

    bump version --> 3.3.21

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
 #Copyright ReportLab Europe Ltd. 2000-2016
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
-Version = "3.3.20"
+Version = "3.3.21"
 __version__=Version
-__date__='20161026'
+__date__='20161116'
 
 import sys, os
 
