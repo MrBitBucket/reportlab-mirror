645dda8c779b2a1786bcb774bfa167a942d7ec28
robin <robin@reportlab.com>
2017-11-15 09:30:28 +0000

    fix pagesizes reported by eliocor @ bitbucket; version --> 3.4.18

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
 #Copyright ReportLab Europe Ltd. 2000-2017
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
-Version = "3.4.17"
+Version = "3.4.18"
 __version__=Version
-__date__='20171106'
+__date__='20171115'
 
 import sys, os
 
