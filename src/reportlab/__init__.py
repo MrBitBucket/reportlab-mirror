81880456de880905af682d560feecfeb19526874
robin <robin@reportlab.com>
2017-12-08 14:34:39 +0000

    fix version

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
 #Copyright ReportLab Europe Ltd. 2000-2017
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
-Version = "3.4.20"
+Version = "3.4.21"
 __version__=Version
-__date__='20171201'
+__date__='20171208'
 
 import sys, os
 
