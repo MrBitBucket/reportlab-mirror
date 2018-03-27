6c5244733e8ad4513b73fc3f8aa47ac5b3d7cd46
robin <robin@reportlab.com>
2018-03-27 15:29:42 +0100

    version --> 3.4.31

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
 #Copyright ReportLab Europe Ltd. 2000-2017
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
-Version = "3.4.30"
+Version = "3.4.31"
 __version__=Version
-__date__='20180323'
+__date__='20180327'
 
 import sys, os
 
