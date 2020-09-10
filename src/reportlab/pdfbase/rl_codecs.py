5836a8d744bdeefae32250c1d9b8f32da554a838
tjj021 <tjj021@126.com>
2020-09-10 14:30:37 +0800

    Fix unicode encoding exception

--- src/reportlab/pdfbase/rl_codecs.py
+++ src/reportlab/pdfbase/rl_codecs.py
@@ -997,7 +997,10 @@ class RL_Codecs:
         ### Codec APIs
         class Codec(codecs.Codec):
             def encode(self,input,errors='strict',charmap_encode=codecs.charmap_encode,encoding_map=encoding_map):
-                return charmap_encode(input,errors,encoding_map)
+                try:
+                    return charmap_encode(input,errors,encoding_map)
+                except Exception as e:
+                    return (input.encode('utf-8'),len(input))
 
             def decode(self,input,errors='strict',charmap_decode=codecs.charmap_decode,decoding_map=decoding_map):
                 return charmap_decode(input,errors,decoding_map)
