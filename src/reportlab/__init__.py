ad3c88f5bbebdb4aa2f0bc6be1ff3be0aeb9bd7c
robin <unknown>
2023-02-22 10:37:27 +0000

    add support for drawImage/drawInlineImage extraReturn dict

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
-#Copyright ReportLab Europe Ltd. 2000-2021
+#Copyright ReportLab Europe Ltd. 2000-2023
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
 Version = "4.0.0"
 __version__=Version
-__date__='20221215'
+__date__='20230222'
 
 import sys, os
 
--- src/reportlab/pdfgen/pdfimages.py
+++ src/reportlab/pdfgen/pdfimages.py
@@ -160,7 +160,8 @@ class PDFImage:
         self.width = self.width or imgwidth
         self.height = self.height or imgheight
 
-    def drawInlineImage(self, canvas, preserveAspectRatio=False,anchor='sw', anchorAtXY=False, showBoundary=False):
+    def drawInlineImage(self, canvas, preserveAspectRatio=False,anchor='sw', anchorAtXY=False,
+                     showBoundary=False, extraReturn=None):
         """Draw an Image into the specified rectangle.  If width and
         height are omitted, they are calculated from the image size.
         Also allow file names as well as images.  This allows a
@@ -172,12 +173,17 @@ class PDFImage:
         # this says where and how big to draw it
         if not canvas.bottomup: y = y+height
         canvas._code.append('q %s 0 0 %s cm' % (fp_str(self.width), fp_str(self.height, x, y)))
+        width = self.width
+        height = self.height
         # self._code.extend(imagedata) if >=python-1.5.2
         for line in self.imageData:
             canvas._code.append(line)
         canvas._code.append('Q')
         if showBoundary:
             canvas.drawBoundary(showBoundary,x,y,width,height)
+        if extraReturn:
+            for k in extraReturn.keys():
+                extraReturn[k] = vars()[k]
         return True
 
     def format(self, document):
