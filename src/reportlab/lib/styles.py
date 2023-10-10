aa9b4d8feb807cdd59089df98b3ef35fc70e5aa7
robin <unknown>
2023-10-10 14:12:38 +0100

    add styles str2alignment maps left/right/center/centre/justify

--- src/reportlab/lib/styles.py
+++ src/reportlab/lib/styles.py
@@ -17,13 +17,14 @@ development, with a few basic heading and text styles.
 __all__=(
         'PropertySet',
         'ParagraphStyle',
+        'str2alignment'
         'LineStyle',
         'ListStyle',
         'StyleSheet1',
         'getSampleStyleSheet',
         )
 from reportlab.lib.colors import black
-from reportlab.lib.enums import TA_LEFT, TA_CENTER
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
 from reportlab.lib.fonts import tt2ps
 from reportlab.rl_config import canvas_basefontname as _baseFontName, \
                                 underlineWidth as _baseUnderlineWidth, \
@@ -158,6 +159,17 @@ class ParagraphStyle(PropertySet):
         'embeddedHyphenation': _embeddedHyphenation,
         'uriWasteReduce': _uriWasteReduce,
         }
+
+def str2alignment(v,__map__=dict(
+                      centre=TA_CENTER,
+                      center=TA_CENTER,
+                      left=TA_LEFT,right=TA_RIGHT,
+                      justify=TA_JUSTIFY)):
+            _ = __map__.get(v.lower(),None)
+            if _ is not None:
+                return _
+            else:
+                raise ValueError(f'{v!r} is illegal value for alignment')
 
 class LineStyle(PropertySet):
     defaults = {
