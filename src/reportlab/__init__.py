c8e409a8441fde54f47cc7fe5eb9bd67f693553c
robin <unknown>
2024-08-23 12:37:24 +0100

    version --> 4.2.3a1

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,9 +1,9 @@
 #Copyright ReportLab Europe Ltd. 2000-2023
 #see license.txt for license details
 __doc__="""The Reportlab PDF generation library."""
-Version = "4.2.2"
+Version = "4.2.3a1"
 __version__=Version
-__date__='20240621'
+__date__='20240822'
 
 import sys, os
 
